bb164c0bd1fc77215263d0741af033e8c2f86481
Tyler Gross <tylergross28@gmail.com>
2019-06-28 16:34:28 -0500

    page wrapper

---
+++ public/src/components/PageWrapper.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="page">
+    <slot></slot>
+  </section>
+</template>
+
+<style scoped>
+.page {
+  padding-top: 55px;
+}
+</style>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({});
+</script>
+
--- public/src/views/Home.vue
+++ public/src/views/Home.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="home">
-    <p>Blank app</p>
-  </div>
+  <page-wrapper>
+    <div class="home">
+      <p>Blank app</p>
+    </div>
+  </page-wrapper>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import PageWrapper from '../components/PageWrapper.vue';
+
+export default Vue.extend({
+  components: {
+    PageWrapper
+  }
+});
+</script>
