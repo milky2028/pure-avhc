8b8e4823195895ba920f918110d66199f1d9d8dd
Tyler <tylergross28@gmail.com>
2019-07-20 17:02:29 -0500

    content done for privacy policy

--- public/src/views/PrivacyPolicy.vue
+++ public/src/views/PrivacyPolicy.vue
@@ -1,6 +1,169 @@
 <template>
   <page-wrapper withPadding>
-    <article-page title="Privacy Policy">Contentssss</article-page>
+    <article-page title="GDPR Privacy Policy">
+      <h2>Other Policies</h2>
+      <p>
+        In addition to this Privacy Policy, please review our
+        <router-link to="/return-policy">Return Policy</router-link>&nbsp;and
+        <router-link to="/terms-and-conditions">Terms and Conditions of Service</router-link>.
+      </p>
+      <h2>Why do we have a privacy policy?</h2>
+      <p>Our privacy policy exists to inform our customers of:</p>
+      <ul>
+        <li>Data we collect during casual use of our website.</li>
+        <li>Data collected when a customer places an order on our website.</li>
+        <li>Services that we use on our website that enable us to deliver a website that provides a high quality, safe, and secure shopping experience.</li>
+        <li>Ensure customers that we value their privacy and work towards creating an experience that does not compromise their information in any way possible.</li>
+      </ul>
+      <p>Your online privacy is important to {{ legalName }} and our partners. We are your ally in ensuring that your information stays safe, secure, and private.</p>
+      <p>
+        <strong class="bold">{{ legalName }}</strong>
+        <em>('we' or 'us' or 'our')</em> is committed to ensuring the security and protection of the personal information that we process, and to provide a compliant and consistent approach to data protection. We have always had a robust and effective data protection program in place which complies with existing law and abides by the data protection principles. However, we recognise our obligations in updating and expanding this program to meet the demands of the
+        <a
+          href="https://eugdpr.org/"
+        >
+          <abbr title="General Data Protection Regulation">GDPR</abbr>
+        </a>.
+      </p>
+      <p></p>
+      <p>
+        <strong class="bold">{{ legalName }}</strong> is dedicated to safeguarding the personal information under our remit and in developing a data protection regime that is effective, fit for purpose and demonstrates an understanding of, and appreciation for the new regulation. Our preparation and objectives for
+        <abbr
+          title="General Data Protection Regulation"
+        >GDPR</abbr> compliance have been summarised in this statement and include the development and implementation of new data protection roles, policies, procedures, controls and measures to ensure maximum and ongoing compliance.
+      </p>
+
+      <h2>
+        <abbr title="General Data Protection Regulation">GDPR</abbr> Roles and Employees
+      </h2>
+      <p>
+        <strong class="bold">{{ legalName }}</strong> has designated Tyler Gross as our
+        <strong class="bold">Data Protection Officer (DPO)</strong> and have appointed a data privacy team to develop and implement our roadmap for complying with the new data protection Regulation. The team are responsible for promoting awareness of the
+        <abbr
+          title="General Data Protection Regulation"
+        >GDPR</abbr> across the organization, assessing our
+        <abbr title="General Data Protection Regulation">GDPR</abbr> readiness, identifying any gap areas and implementing the new policies, procedures and measures.
+      </p>
+      <p>
+        We understand that continuous employee awareness and understanding is vital to the continued compliance of the
+        <abbr
+          title="General Data Protection Regulation"
+        >GDPR</abbr> and have involved our employees in our preparation plans. We have implemented an employee training program specific to the which will be provided to all employees and forms part of our induction and annual training program.
+      </p>
+
+      <h2>Key Terms</h2>
+      <p>
+        <strong class="bold">User/Customer:</strong>Any individual who visits
+        <a :href="siteUrl">{{ siteUrl }}</a> via a web browser or its associated domains on any number of devices, such as a laptop, smartphone, desktop computer, smartwatch, or other internet-connected device.
+      </p>
+
+      <h2>What do we do with your information?</h2>
+      <p>When you purchase something from our store, as part of the buying and selling process, we collect the personal information you give us such as your name, address and email address. In addition, we automatically collect information that helps us understand the machine (smartphone, tablet, desktop pc, laptop, etc) you used to connect with our website. This information helps us understand the machine you used and create a better-optimized site that meets the features available to your system and its hardware.</p>
+      <p>With your permission, we may send you emails about our store, new products and other updates.</p>
+
+      <h2>How do you get my consent?</h2>
+      <p>When you provide us with personal information to complete a transaction, verify your credit card, place an order, arrange for a delivery or return a purchase, we imply that you consent to our collecting it and using it for that specific reason only.</p>
+      <p>If we ask for your personal information for a secondary reason, like marketing, we will either ask you directly for your expressed consent, or provide you with an opportunity to say no.</p>
+
+      <h2>How do I withdraw my consent?</h2>
+      <p>
+        You may withdraw your consent at any time by emailing
+        <a
+          href="mailto:support@purecbdexchange.com?subject=Please%20remove%20my%20data&body=Hello!%0A%0APlease%20remove%20all%20my%20information%20from%20your%20records.%0A%0AThank%20you!"
+        >support@purecbdexchange.com</a>.
+      </p>
+
+      <h2>Third Party Services</h2>
+      <p>In general, the third-party providers used by us will only collect, use and disclose your information to the extent necessary to allow them to perform the services they provide to us.</p>
+      <p>However, certain third-party service providers, such as payment gateways and other payment transaction processors, have their own privacy policies in respect to the information we are required to provide to them for your purchase-related transactions.</p>
+      <p>For these providers, we recommend that you read their privacy policies so you can understand the manner in which your personal information will be handled by these providers.</p>
+      <p>In particular, remember that certain providers may be located in or have facilities that are located a different jurisdiction than either you or us. So if you elect to proceed with a transaction that involves the services of a third-party service provider, then your information may become subject to the laws of the jurisdiction(s) in which that service provider or its facilities are located.</p>
+      <p>
+        As an example, if you are located in Canada and your transaction is processed by a payment gateway located in the United States, then your personal information used in completing that transaction may be subject to disclosure under United States legislation, including the
+        <a
+          href="https://en.wikipedia.org/wiki/Patriot_Act"
+        >
+          <abbr
+            title="Uniting and Strengthening America by Providing Appropriate Tools Required to Intercept and Obstruct Terrorism"
+          >U.S.A. P.A.T.R.I.O.T Act</abbr>
+        </a>.
+      </p>
+      <p>
+        Once you leave our store's website or are redirected to a third-party website or application, you are no longer governed by this Privacy Policy or our website's
+        <router-link to="/terms-and-conditions">Terms and Conditions</router-link>.
+      </p>
+
+      <h2>Links</h2>
+      <p>When you click on links on our store, they may direct you away from our site. We are not responsible for the privacy practices of other sites and encourage you to read their privacy statements.</p>
+      <p>
+        This site recognizes and collects, when possible, the domain name of a visitor's server (for example,
+        <a
+          class="link"
+          href="https://purecbdexchange.com"
+        >purecbdexchange.com</a> or
+        <a class="link" href="https://google.com">google.com</a>). We do not automatically collect full email addresses of visitors to our website. The only way we obtain your name or email address is when you choose to provide this information to us, such as when you make purchase or subscribe to a email list. Examples of how you might provide us with such personal information include:
+      </p>
+      <ul>
+        <li>Completing a survey or feedback form</li>
+        <li>Emailing us with a comment or question</li>
+        <li>Subscribing to email newsletters</li>
+        <li>Using our patient portal</li>
+        <li>Live-chatting our representatives with questions</li>
+        <li>Submitting a form to schedule a home visit</li>
+      </ul>
+      <p>
+        We use
+        <a class="link" href="https://analytics.google.com">Google Universal Analytics</a> to collect website data that allows us to get a better understanding of how our site visitors interact with our online content. If you do nothing during your visit but browse through the website or download information, Google Universal Analytics will automatically gather and store certain information about your visit. This information does not identify you personally and is used in a general way to help us improve our website and tell us the number of visitors to our site each day.
+      </p>
+      <p>Our web client automatically collects and records the following information:</p>
+      <ul>
+        <li>Aggregate information on what pages are accessed</li>
+        <li>Address of the website that linked to us (referral URL)</li>
+        <li>Date and time you access our site</li>
+        <li>Name and release number of web browser software used</li>
+        <li>Operating system used</li>
+        <li>Visitor's domain name, but not the email address</li>
+        <li>Visitor's IP address</li>
+        <li>Age, gender, and interests</li>
+      </ul>
+      <p>In addition to Google Analytics, we use a service called Sentry to provide us with information about code errors that may have occurred during your browsing experience. Much like Google Analytics, Sentry keeps data collected from visitors anonymous and the data collected is only used to improve your website experience. We also use Google Tag Manager to collect analytics data about internal link clicks and other website properties.</p>
+      <p>Other Services Used:</p>
+      <ul>
+        <li>
+          <a class="link" href="https://firebase.google.com">Firebase</a>
+        </li>
+        <li>
+          <a class="link" href="https://shopify.com">Shopify</a>
+        </li>
+        <li>
+          <a class="link" href="https://sentry.io">Sentry Error Reporting</a>
+        </li>
+      </ul>
+
+      <h2>Security</h2>
+      <p>To protect your personal information, we take reasonable precautions and follow industry best practices to make sure it is not inappropriately lost, misused, accessed, disclosed, altered or destroyed.</p>
+      <p>
+        If you provide us with your credit card information, the information is encrypted using secure socket layer technology (SSL) and stored with a AES-256 encryption. Although no method of transmission over the Internet or electronic storage is 100% secure, we follow all PCI-DSS requirements and implement additional generally accepted industry standards. For security during the transmission of webpages and to prevent man-in-the-middle attacks, our site is only served over
+        <abbr
+          title="Secure Hyper Text Transfer Protocl"
+        >HTTPS</abbr>.
+      </p>
+
+      <h2>Age of Consent</h2>
+      <p>By using this site, you represent that you are at least the age of majority in your state or province of residence, or that you are the age of majority in your state or province of residence and you have given us your consent to allow any of your minor dependents to use this site.</p>
+
+      <h2>Change to This Privacy Policy</h2>
+      <p>We reserve the right to modify this privacy policy at any time, so please review it frequently. Changes and clarifications will take effect immediately upon their posting on the website. If we make material changes to this policy, we will notify you here that it has been updated, so that you are aware of what information we collect, how we use it, and under what circumstances, if any, we use and/or disclose it. If our store is acquired or merged with another company, your information may be transferred to the new owners so that we may continue to sell products to you.</p>
+
+      <h2>Questions and Contact Information</h2>
+      <p>
+        If you would like to access, correct, amend or delete any personal information we have about you, register a complaint, or simply want more information, please contact us by emailing
+        <a
+          class="link"
+          href="mailto:support@purecbdexchange.com?subject=Hello!%20I%20want%20to%20learn%20more%20about%20your%20privacy%20policy&body=Faces...%20Tris3"
+        >support@purecbdexchange.com</a>.
+      </p>
+    </article-page>
   </page-wrapper>
 </template>
 
@@ -10,6 +173,12 @@ import PageWrapper from '../components/PageWrapper.vue';
 import ArticlePage from '../components/ArticlePage.vue';
 
 export default Vue.extend({
+  data() {
+    return {
+      legalName: process.env.VUE_APP_LEGAL_NAME,
+      siteUrl: process.env.VUE_APP_SITE_URL
+    };
+  },
   components: {
     PageWrapper,
     ArticlePage
