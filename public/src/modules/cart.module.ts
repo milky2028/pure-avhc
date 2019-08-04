ed8151e6eafd6fd501801e01f9d5aabc5e627170
Tyler <tylergross28@gmail.com>
2019-08-04 12:10:46 -0500

    getters done for cart module

--- public/src/modules/cart.module.ts
+++ public/src/modules/cart.module.ts
@@ -1,7 +1,24 @@
+import CartItem from '@/types/CartItem';
+
+interface CartModuleState {
+  cartItems: CartItem[];
+}
+
 const CartModule = {
   namespaced: true,
   state: {
     cartItems: []
+  },
+  getters: {
+    computedQuantity: (state: CartModuleState) =>
+      state.cartItems.length > 0
+        ? state.cartItems.reduce((acc, { quantity }) => acc + quantity, 0)
+        : 0,
+    subtotal: (state: CartModuleState) =>
+      state.cartItems.reduce(
+        (acc, { quantity, price }) => acc + quantity * price,
+        0
+      )
   }
 };
 
