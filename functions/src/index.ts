4f6b3221610edc972b776fbb57213347bfe62855
Tyler Gross <tylergross28@gmail.com>
2019-08-29 17:25:06 -0500

    mailchimp subscribe function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,32 @@
 import * as functions from 'firebase-functions';
+import axios, { AxiosRequestConfig } from 'axios';
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
+export const addEmailToList = functions.https.onRequest(async (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET');
+  res.set('Access-Control-Allow-Headers', 'Content-Type');
+  const apiKey = functions.config().mailchimp.key;
+  const config: AxiosRequestConfig = {
+    headers: {
+      'content-type': 'application/json'
+    },
+    auth: {
+      username: 'tyler',
+      password: apiKey
+    }
+  };
+
+  const data = {
+    email_address: req.body.email,
+    status: 'subscribed',
+    merge_fields: {}
+  };
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  const url = 'https://us15.api.mailchimp.com/3.0/lists/fdbb4c13c1/members';
+  await axios.post(url, data, config).catch((e) => console.error(e));
+  return res.sendStatus(200);
 });
