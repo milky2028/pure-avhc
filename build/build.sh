f84a968d070d9953249f09771646ed18da7a50df
Tyler Gross <tylergross28@gmail.com>
2019-09-05 17:41:35 -0500

    most steps should work except firebase

--- build/build.sh
+++ build/build.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
 
-echo "Hello!"
-pwd
-ls
+error_exit() {
+	echo "Error! $1" 1>&2
+	exit 1
+}
 
-# error_exit() {
-# 	echo "Error! $1" 1>&2
-# 	exit 1
-# }
+if [ -z "$1" ]; then 
+  error_exit "Failed to provide a build target"
+fi
 
-# if [ -z "$1" ]; then 
-#   error_exit "Failed to provide a build target"
-# fi
+BUILD_TARGET=$1
 
-# BUILD_TARGET=$1
+echo "Install build modules"
+cd ${GITHUB_WORKSPACE}/build
+yarn install
 
-# echo "Install build modules"
-# yarn install
+echo "Install functions modules"
+cd ${GITHUB_WORKSPACE}/functions
+yarn install
 
-# echo "Install functions modules"
-# cd ${GITHUB_WORKSPACE}/functions
-# yarn install
+echo "Install client modules"
+cd ${GITHUB_WORKSPACE}/public
+yarn install
 
-# echo "Install client modules"
-# cd ${GITHUB_WORKSPACE}/public
-# yarn install
+echo "Build with Vue"
+yarn build:${BUILD_TARGET}
 
-# echo "Build with Vue"
-# yarn build:${BUILD_TARGET}
-
-# echo "Deploy to firebase"
-# cd ${GITHUB_WORKSPACE}/public
-# firebase use ${BUILD_TARGET}
-# firebase deploy
+echo "Deploy to firebase"
+cd ${GITHUB_WORKSPACE}
+firebase use ${BUILD_TARGET}
+firebase deploy
