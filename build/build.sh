1eb927927330bb9dae540f0c77cac081c5dcfd80
Tyler Gross <tylergross28@gmail.com>
2019-09-05 17:10:36 -0500

    do the build and update the build with notice statements

--- build/build.sh
+++ build/build.sh
@@ -17,24 +17,31 @@ cd ..
 git status
 git pull
 
+echo "Downloading prerequisite global modules"
 yarn global add firebase-tools @vue/cli @vue/cli-service-global typescript
 rm -rf public/node_modules functions/node_modules build/node_modules
 
+echo "Install build modules"
 cd build
 rm -rf node_modules
 yarn install
 
+echo "Install functions modules"
 nvm install 8
 cd ../functions
 rm -rf node_modules
 yarn install
 
+echo "Install client modules"
 nvm install 10
 cd ../public
 rm -rf node_modules
 yarn install
+
+echo "Build with Vue"
 yarn build:${BUILD_TARGET}
 
+echo "Deploy to firebase"
 cd ..
 firebase use ${BUILD_TARGET}
 firebase deploy
