e4006ebddc54a01e4974f29949014d789fd740cd
Tyler Gross <tylergross28@gmail.com>
2019-09-05 17:36:01 -0500

    add build deps

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-. ~/.nvm/nvm.sh
 
 error_exit() {
 	echo "Error! $1" 1>&2
@@ -12,36 +11,21 @@ fi
 
 BUILD_TARGET=$1
 
-cd ..
-
-git status
-git pull
-
-echo "Downloading prerequisite global modules"
-yarn global add firebase-tools @vue/cli @vue/cli-service-global typescript
-rm -rf public/node_modules functions/node_modules build/node_modules
-
 echo "Install build modules"
-cd build
-rm -rf node_modules
 yarn install
 
 echo "Install functions modules"
-nvm install 8
-cd ../functions
-rm -rf node_modules
+cd ${GITHUB_WORKSPACE}/functions
 yarn install
 
 echo "Install client modules"
-nvm install 10
-cd ../public
-rm -rf node_modules
+cd ${GITHUB_WORKSPACE}/public
 yarn install
 
 echo "Build with Vue"
 yarn build:${BUILD_TARGET}
 
 echo "Deploy to firebase"
-cd ..
-firebase use ${BUILD_TARGET}
-firebase deploy
+cd ${GITHUB_WORKSPACE}/public
+# firebase use ${BUILD_TARGET}
+# firebase deploy
