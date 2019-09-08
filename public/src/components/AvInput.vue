7656e32795677975cd79ab42a7f0364a469df083
Tyler Gross <tylergross28@gmail.com>
2019-09-08 17:58:06 -0500

    subscribe user flow done

--- public/src/components/AvInput.vue
+++ public/src/components/AvInput.vue
@@ -1,16 +1,17 @@
 <template>
-  <div>
-    <label v-if="label" :for="fieldId">{{ label }}</label>
+  <label :for="fieldId">
+    {{ label }}
     <input
+      :id="fieldId"
+      v-bind="$attrs"
       @input="$emit('on-input', $event.target.value)"
       @keyup.enter="$emit('enter', $event.target.value)"
       @blur="dirty = true"
-      :value="inputValue"
-      :placeholder="placeholder"
-      :class="{ darkBackground, dirty }"
+      :value="value"
+      :class="{ darkBackground, dirty, error }"
       :style="{ width }"
     />
-  </div>
+  </label>
 </template>
 
 <style scoped>
@@ -18,28 +19,41 @@ input {
   background-color: white;
   padding: 10px;
   border-radius: var(--rounded-corner);
+  border: 1.3px solid;
+  border-color: var(--dark-accent);
 }
 
 .darkBackground {
   color: white;
   background-color: var(--primary-color);
 }
+
+.error,
+input.dirty:invalid {
+  border-color: var(--warn);
+}
 </style>
 
 <script lang="ts">
 import Vue from 'vue';
+import CreateRandomId from '../actors/createRandomId';
+
 export default Vue.extend({
+  inheritAttrs: false,
   props: {
-    placeholder: String,
     label: String,
     darkBackground: Boolean,
     width: String,
-    inputValue: String
+    value: String,
+    error: Boolean
   },
   data() {
     return {
       dirty: false
     };
+  },
+  computed: {
+    fieldId: () => CreateRandomId()
   }
 });
 </script>
--- public/src/components/CheckoutPrompt.vue
+++ public/src/components/CheckoutPrompt.vue
@@ -4,18 +4,34 @@
       <h2 class="subhead checkout">
         <router-link to="/cart">Checkout ></router-link>
       </h2>
+      <av-icon-button
+        v-if="windowWidth < 825 && expanded"
+        class="icon"
+        @icon-click="expanded = false"
+      >close</av-icon-button>
       <transition name="fade">
         <av-input
           class="input"
           v-if="canSubscribe && windowWidth > 825 || canSubscribe && expanded"
           @enter="onSubscribe(email)"
           @on-input="email = $event"
+          :value="email"
           width="100%"
-          :inputValue="email"
+          :error="formError"
+          :pattern="emailPattern"
           placeholder="Subscribe via email and get 10% off today"
+          required
         ></av-input>
       </transition>
-      <av-button v-if="canSubscribe" class="btn" @btn-click="onSubscribe(email)">{{ btnText }}</av-button>
+      <transition name="fade">
+        <av-button v-if="canSubscribe" class="btn" @btn-click="onSubscribe(email)">{{ btnText }}</av-button>
+      </transition>
+      <transition name="fade">
+        <p v-if="formError" class="body-text error">{{ errorMsg }}</p>
+      </transition>
+      <transition name="fade">
+        <p v-if="subscribed" class="body-text msg">Subscribed!</p>
+      </transition>
     </div>
   </transition>
 </template>
@@ -31,16 +47,21 @@
   width: 100%;
   box-shadow: var(--basic-shadow);
   display: grid;
-  grid-template-areas: 'input btn checkout';
-  grid-template-columns: 350px 103px 1fr;
+  grid-template-areas: 'input btn msg checkout';
+  grid-template-columns: 350px 140px 1fr 1fr;
   grid-column-gap: 1vw;
   align-items: center;
   overflow: hidden;
   transition: all 300ms var(--mat-ease);
 }
 
+.input {
+  grid-area: input;
+}
+
 .btn {
   grid-area: btn;
+  min-width: 140px;
 }
 
 .checkout {
@@ -49,20 +70,37 @@
   justify-self: end;
 }
 
+.msg {
+  justify-self: start;
+  grid-area: input;
+  color: white;
+}
+
+.error {
+  grid-area: msg;
+  color: var(--warn);
+}
+
 @media (max-width: 825px) {
   .prompt {
     grid-template-columns: 1fr 1fr;
     grid-template-areas:
+      'msg icon'
       'input input'
       'btn checkout';
   }
 
-  .input {
-    grid-area: input;
+  .expanded {
+    height: 140px;
   }
 
-  .expanded {
-    height: 110px;
+  .icon {
+    justify-self: end;
+    grid-area: icon;
+  }
+
+  .msg {
+    grid-area: btn;
   }
 }
 </style>
@@ -72,42 +110,70 @@ import Vue from 'vue';
 import { mapState } from 'vuex';
 import AvInput from '../components/AvInput.vue';
 import AvButton from '../components/AvButton.vue';
+import AvIconButton from '../components/AvIconButton.vue';
+import { setTimeout } from 'timers';
+import * as idb from 'idb-keyval';
 
 export default Vue.extend({
   components: {
     AvInput,
-    AvButton
-  },
-  computed: {
-    ...mapState('cart', ['cartItems']),
-    btnText() {
-      return this.windowWidth > 825 || this.expanded
-        ? 'Subscribe'
-        : 'Get 10% Off';
-    }
+    AvButton,
+    AvIconButton
   },
   data() {
     return {
       email: '',
       expanded: false,
       windowWidth: window.innerWidth,
-      canSubscribe: true
+      canSubscribe: true,
+      emailPattern: '^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$',
+      formError: false,
+      errorMsg: 'Invalid email format',
+      btnText: window.innerWidth > 825 ? 'Subscribe' : 'Get 10% Off',
+      subscribed: false
     };
   },
-  beforeMount() {
-    window.addEventListener(
-      'resize',
-      () => (this.windowWidth = window.innerWidth)
-    );
+  computed: {
+    ...mapState('cart', ['cartItems'])
+  },
+  async beforeMount() {
+    window.addEventListener('resize', () => {
+      this.windowWidth = window.innerWidth;
+      if (this.windowWidth < 825) {
+        this.btnText = 'Get 10% Off';
+      } else {
+        this.btnText = 'Subscribe';
+      }
+    });
+
+    // const idbCanSubscribe = await idb.get('canSubscribe');
+    // this.canSubscribe = idbCanSubscribe === undefined ? true : idbCanSubscribe;
   },
   methods: {
-    onSubscribe(event: string) {
+    onSubscribe(email: string) {
       if (this.windowWidth > 825 || this.expanded) {
-        console.log(event);
-        this.email = '';
-        this.expanded = false;
+        const reg = new RegExp(this.emailPattern);
+        const isValid = reg.test(email);
+        if (isValid) {
+          this.btnText = 'Subscribing...';
+          this.formError = false;
+
+          // TODO: Replace this with actual API call promise
+          setTimeout(() => {
+            console.log(email);
+            this.email = '';
+            this.expanded = false;
+            this.canSubscribe = false;
+            this.subscribed = true;
+            setTimeout(() => (this.subscribed = false), 2500);
+            idb.set('canSubscribe', false);
+          }, 3000);
+        } else {
+          this.formError = true;
+        }
       } else {
         this.expanded = true;
+        this.btnText = 'Subscribe';
       }
     }
   }
