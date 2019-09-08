4dc061f2d621d5b150e3df35c0e6e6a7ae171ea3
Tyler Gross <tylergross28@gmail.com>
2019-09-08 14:24:19 -0500

    mobile styles for checkout prompt

--- public/src/components/CheckoutPrompt.vue
+++ public/src/components/CheckoutPrompt.vue
@@ -1,18 +1,21 @@
 <template>
   <transition name="slide-y">
-    <div class="prompt" v-if="cartItems && cartItems.length > 0">
+    <div class="prompt" :class="{ expanded }" v-if="cartItems && cartItems.length > 0">
       <h2 class="subhead checkout">
         <router-link to="/cart">Checkout ></router-link>
       </h2>
-      <div class="subscribe">
+      <transition name="fade">
         <av-input
-          @enter="onSubscribe($event)"
+          class="input"
+          v-if="canSubscribe && windowWidth > 825 || canSubscribe && expanded"
+          @enter="onSubscribe(email)"
           @on-input="email = $event"
-          width="350px"
-          placeholder="Email: Get 10% off today"
+          width="100%"
+          :inputValue="email"
+          placeholder="Subscribe via email and get 10% off today"
         ></av-input>
-        <av-button @btn-click="onSubscribe(email)">Subscribe</av-button>
-      </div>
+      </transition>
+      <av-button v-if="canSubscribe" class="btn" @btn-click="onSubscribe(email)">{{ btnText }}</av-button>
     </div>
   </transition>
 </template>
@@ -28,23 +31,39 @@
   width: 100%;
   box-shadow: var(--basic-shadow);
   display: grid;
-  grid-template-areas: 'main';
+  grid-template-areas: 'input btn checkout';
+  grid-template-columns: 350px 103px 1fr;
+  grid-column-gap: 1vw;
   align-items: center;
+  overflow: hidden;
+  transition: all 300ms var(--mat-ease);
+}
+
+.btn {
+  grid-area: btn;
 }
 
 .checkout {
-  grid-area: main;
+  grid-area: checkout;
   color: white;
   justify-self: end;
 }
 
-.subscribe {
-  grid-area: main;
-  justify-self: start;
-  grid-area: main;
-  display: grid;
-  grid-auto-flow: column;
-  grid-column-gap: 1vw;
+@media (max-width: 825px) {
+  .prompt {
+    grid-template-columns: 1fr 1fr;
+    grid-template-areas:
+      'input input'
+      'btn checkout';
+  }
+
+  .input {
+    grid-area: input;
+  }
+
+  .expanded {
+    height: 110px;
+  }
 }
 </style>
 
@@ -60,16 +79,36 @@ export default Vue.extend({
     AvButton
   },
   computed: {
-    ...mapState('cart', ['cartItems'])
+    ...mapState('cart', ['cartItems']),
+    btnText() {
+      return this.windowWidth > 825 || this.expanded
+        ? 'Subscribe'
+        : 'Get 10% Off';
+    }
   },
   data() {
     return {
-      email: ''
+      email: '',
+      expanded: false,
+      windowWidth: window.innerWidth,
+      canSubscribe: true
     };
   },
+  beforeMount() {
+    window.addEventListener(
+      'resize',
+      () => (this.windowWidth = window.innerWidth)
+    );
+  },
   methods: {
-    onSubscribe(event: any) {
-      console.log(event);
+    onSubscribe(event: string) {
+      if (this.windowWidth > 825 || this.expanded) {
+        console.log(event);
+        this.email = '';
+        this.expanded = false;
+      } else {
+        this.expanded = true;
+      }
     }
   }
 });
