24c3d9607f6d0a3d1f494ca3c683da75292cbd4c
Tyler Gross <tylergross28@gmail.com>
2019-09-11 22:15:56 -0500

    add to mailchimp on doc create

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,32 +1,38 @@
 import * as functions from 'firebase-functions';
 import axios, { AxiosRequestConfig } from 'axios';
 
-export const addEmailToList = functions.https.onRequest(async (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Methods', 'GET');
-  res.set('Access-Control-Allow-Headers', 'Content-Type');
-  const apiKey = functions.config().mailchimp.key;
-  const config: AxiosRequestConfig = {
-    headers: {
-      'content-type': 'application/json'
-    },
-    auth: {
-      username: 'tyler',
-      password: apiKey
+export const addEmailToList = functions.firestore
+  .document('subscribers/{subscriberId}')
+  .onCreate(async (snap) => {
+    const documentData = snap.data();
+    if (documentData) {
+      const apiKey = functions.config().mailchimp.key;
+      console.log(documentData.email);
+
+      const config: AxiosRequestConfig = {
+        headers: {
+          'content-type': 'application/json'
+        },
+        auth: {
+          username: 'tyler',
+          password: apiKey
+        }
+      };
+
+      const payload = {
+        email_address: documentData.email,
+        status: 'subscribed',
+        merge_fields: {}
+      };
+
+      try {
+        const url =
+          'https://us15.api.mailchimp.com/3.0/lists/fdbb4c13c1/members';
+        return await axios.post(url, payload, config);
+      } catch (e) {
+        throw new Error(e);
+      }
+    } else {
+      throw new Error('No document data');
     }
-  };
-
-  const data = {
-    email_address: req.body.email,
-    status: 'subscribed',
-    merge_fields: {}
-  };
-
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204);
-  }
-
-  const url = 'https://us15.api.mailchimp.com/3.0/lists/fdbb4c13c1/members';
-  await axios.post(url, data, config).catch((e) => console.error(e));
-  return res.sendStatus(200);
-});
+  });
