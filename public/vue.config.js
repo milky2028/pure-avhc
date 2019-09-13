c5a4218d0c779006aeed7bb30d3e386a53f72b65
Tyler Gross <tylergross28@gmail.com>
2019-09-12 21:54:43 -0500

    spa prerender some pages

--- public/vue.config.js
+++ public/vue.config.js
@@ -1,21 +1,24 @@
-// const Donenv = require('dotenv-webpack');
-// const path = require('path');
-// const PrerenderSpaPlugin = require('prerender-spa-plugin');
+const path = require('path');
+const PrerenderSpaPlugin = require('prerender-spa-plugin');
 
-// const productionPlugins = [
-//   new PrerenderSpaPlugin({
-//     staticDir: path.join(__dirname, 'dist'),
-//     routes: []
-//   }),
-//   new Dotenv()
-// ];
+const productionPlugins = [
+  new PrerenderSpaPlugin({
+    staticDir: path.join(__dirname, 'dist'),
+    routes: [
+      '/support',
+      '/return-policy',
+      '/terms-and-conditions',
+      '/privacy-policy'
+    ]
+  })
+];
 
 module.exports = {
-  // configureWebpack: config => {
-  //   if (process.env.NODE_ENV === 'production') {
-  //     config.plugins.push(...productionPlugins);
-  //   }
-  // },
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(...productionPlugins);
+    }
+  },
   chainWebpack: (config) => {
     config.module.rule('js').exclude.add(/\.worker\.js$/);
     config.output.globalObject('self');
