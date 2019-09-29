aa25603c29cb329fc0c6f7b118c512c158f19349
Tyler Gross <tylergross28@gmail.com>
2019-09-28 19:41:16 -0500

    alternative sign in buttons styles done

--- public/src/views/Login.vue
+++ public/src/views/Login.vue
@@ -41,6 +41,25 @@
         class="btn"
         @btn-click="onLogin"
       >Login</av-button>
+      <div class="sign-in-container">
+        <button class="sign-in-btn">
+          <img
+            class="sign-in-icon google"
+            src="https://storage.googleapis.com/aspe-f5783.appspot.com/icons/google.svg"
+            alt="Google icon"
+          />Sign in with Google
+        </button>
+        <button class="sign-in-btn facebook">
+          <span>
+            <img
+              class="sign-in-icon"
+              src="https://storage.googleapis.com/aspe-f5783.appspot.com/icons/facebook.svg"
+              alt="Facebook icon"
+            />
+          </span>
+          <span>Sign in with Facebook</span>
+        </button>
+      </div>
     </article-page>
   </page-wrapper>
 </template>
@@ -67,9 +86,58 @@ p {
   margin-top: 15px;
 }
 
+.sign-in-container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.sign-in-btn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 270px;
+  margin-top: 15px;
+  height: 40px;
+  border-radius: var(--rounded-corner);
+  box-shadow: var(--basic-shadow);
+  font-weight: bold;
+}
+
+.sign-in-icon {
+  width: 18px;
+  margin-right: 15px;
+  padding-top: 2px;
+}
+
+.google {
+  width: 23px;
+}
+
+.facebook {
+  color: white;
+  background-color: #3b5998;
+}
+
+img {
+  border-radius: 4px;
+}
+
+.sign-in-btn:active {
+  filter: brightness(90%);
+}
+
 @media (max-width: 825px) {
   .field {
     width: 100%;
+  }
+
+  .sign-in-btn {
+    width: 100%;
+  }
+
+  .sign-in-container {
+    align-items: center;
   }
 }
 </style>
