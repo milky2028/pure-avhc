622f466746be89090e3ca5a06dbbf75f5e1e6609
Tyler Gross <tylergross28@gmail.com>
2019-10-01 15:13:18 -0500

    basic start to shipping form

--- public/src/components/ShippingForm.vue
+++ public/src/components/ShippingForm.vue
@@ -1,9 +1,41 @@
 <template>
-  <form>My shipping form</form>
+  <form>
+    <av-input required dark placeholder="Name" :value="name" @on-input="name = $event"></av-input>
+    <av-input
+      v-if="includeCompany"
+      required
+      dark
+      placeholder="Company"
+      :value="company"
+      @on-input="company = $event"
+    ></av-input>
+  </form>
 </template>
+
+<style scoped>
+form {
+  display: grid;
+  grid-auto-flow: row;
+  grid-row-gap: 1vmax;
+}
+</style>
 
 <script lang="ts">
 import Vue from 'vue';
+import AvInput from './AvInput.vue';
 
-export default Vue.extend({});
+export default Vue.extend({
+  components: {
+    AvInput
+  },
+  props: {
+    includeCompany: Boolean
+  },
+  data() {
+    return {
+      name: '',
+      company: ''
+    };
+  }
+});
 </script>
