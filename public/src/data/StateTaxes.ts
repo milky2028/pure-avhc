f77cd28c0ceed7a4730f8b50204ff695b4c8513f
Tyler <tylergross28@gmail.com>
2019-10-06 13:03:53 -0500

    add state taxes

---
+++ public/src/data/StateTaxes.ts
@@ -0,0 +1,264 @@
+const StateTaxes = [
+  {
+    abbr: 'AL',
+    state: 'Alabama',
+    taxRate: 4.0
+  },
+  {
+    abbr: 'AK',
+    state: 'Alaska',
+    taxRate: 0.0
+  },
+  {
+    abbr: 'AZ',
+    state: 'Arizona',
+    taxRate: 5.6
+  },
+  {
+    abbr: 'AR',
+    state: 'Arkansas',
+    taxRate: 6.5
+  },
+  {
+    abbr: 'CA',
+    state: 'California',
+    taxRate: 7.25
+  },
+  {
+    abbr: 'CO',
+    state: 'Colorado',
+    taxRate: 2.9
+  },
+  {
+    abbr: 'CT',
+    state: 'Connecticut',
+    taxRate: 6.35
+  },
+  {
+    abbr: 'DE',
+    state: 'Delaware',
+    taxRate: 0.0
+  },
+  {
+    abbr: 'FL',
+    state: 'Florida',
+    taxRate: 6.0
+  },
+  {
+    abbr: 'GA',
+    state: 'Georgia',
+    taxRate: 4.0
+  },
+  {
+    abbr: 'HI',
+    state: 'Hawaii',
+    taxRate: 4.0
+  },
+  {
+    abbr: 'ID',
+    state: 'Idaho',
+    taxRate: 6.0
+  },
+  {
+    abbr: 'IL',
+    state: 'Illinois',
+    taxRate: 6.25
+  },
+  {
+    abbr: 'IN',
+    state: 'Indiana',
+    taxRate: 7.0
+  },
+  {
+    abbr: 'IA',
+    state: 'Iowa',
+    taxRate: 6.0
+  },
+  {
+    abbr: 'KS',
+    state: 'Kansas',
+    taxRate: 6.5
+  },
+  {
+    abbr: 'KY',
+    state: 'Kentucky',
+    taxRate: 6.0
+  },
+  {
+    abbr: 'LA',
+    state: 'Louisiana',
+    taxRate: 5.0
+  },
+  {
+    abbr: 'ME',
+    state: 'Maine',
+    taxRate: 5.5
+  },
+  {
+    abbr: 'MD',
+    state: 'Maryland',
+    taxRate: 6.0
+  },
+  {
+    abbr: 'MA',
+    state: 'Massachusetts',
+    taxRate: 6.25
+  },
+  {
+    abbr: 'MI',
+    state: 'Michigan',
+    taxRate: 6.0
+  },
+  {
+    abbr: 'MN',
+    state: 'Minnesota',
+    taxRate: 6.88
+  },
+  {
+    abbr: 'MS',
+    state: 'Mississippi',
+    taxRate: 7.0
+  },
+  {
+    abbr: 'MO',
+    state: 'Missouri',
+    taxRate: 4.23
+  },
+  {
+    abbr: 'MT',
+    state: 'Montana',
+    taxRate: 0.0
+  },
+  {
+    abbr: 'NE',
+    state: 'Nebraska',
+    taxRate: 5.5
+  },
+  {
+    abbr: 'NV',
+    state: 'Nevada',
+    taxRate: 6.85
+  },
+  {
+    abbr: 'NH',
+    state: 'New Hampshire',
+    taxRate: 0.0
+  },
+  {
+    abbr: 'NJ',
+    state: 'New Jersey',
+    taxRate: 6.63
+  },
+  {
+    abbr: 'NM',
+    state: 'New Mexico',
+    taxRate: 5.13
+  },
+  {
+    abbr: 'Ny',
+    state: 'New York',
+    taxRate: 4.0
+  },
+  {
+    abbr: 'NC',
+    state: 'North Carolina',
+    taxRate: 4.75
+  },
+  {
+    abbr: 'ND',
+    state: 'North Dakota',
+    taxRate: 5.0
+  },
+  {
+    abbr: 'OH',
+    state: 'Ohio',
+    taxRate: 5.75
+  },
+  {
+    abbr: 'OK',
+    state: 'Oklahoma',
+    taxRate: 4.5
+  },
+  {
+    abbr: 'OR',
+    state: 'Oregon',
+    taxRate: 0.0
+  },
+  {
+    abbr: 'PA',
+    state: 'Pennsylvania',
+    taxRate: 6.0
+  },
+  {
+    abbr: 'RI',
+    state: 'Rhode Island',
+    taxRate: 7.0
+  },
+  {
+    abbr: 'SC',
+    state: 'South Carolina',
+    taxRate: 6.0
+  },
+  {
+    abbr: 'SD',
+    state: 'South Dakota',
+    taxRate: 4.5
+  },
+  {
+    abbr: 'TN',
+    state: 'Tennessee',
+    taxRate: 7.0
+  },
+  {
+    abbr: 'TX',
+    state: 'Texas',
+    taxRate: 6.25
+  },
+  {
+    abbr: 'UT',
+    state: 'Utah',
+    taxRate: 5.95
+  },
+  {
+    abbr: 'VT',
+    state: 'Vermont',
+    taxRate: 6.0
+  },
+  {
+    abbr: 'VA',
+    state: 'Virginia',
+    taxRate: 5.3
+  },
+  {
+    abbr: 'WA',
+    state: 'Washington',
+    taxRate: 6.5
+  },
+  {
+    abbr: 'WV',
+    state: 'West Virginia',
+    taxRate: 6.0
+  },
+  {
+    abbr: 'WI',
+    state: 'Wisconsin',
+    taxRate: 5.0
+  },
+  {
+    abbr: 'WY',
+    state: 'Wyoming',
+    taxRate: 4.0
+  },
+  {
+    abbr: 'DC',
+    state: 'D.C.',
+    taxRate: 5.75
+  },
+  {
+    abbr: 'NA',
+    state: 'Not Found',
+    taxRate: 5.11
+  }
+];
+
+export default StateTaxes;
