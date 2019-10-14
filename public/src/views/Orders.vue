f53ef47d5ff071abf3f7adfa85159105996fb10c
Tyler <tylergross28@gmail.com>
2019-10-14 17:48:19 -0500

    sign out functionality

--- public/src/views/Orders.vue
+++ public/src/views/Orders.vue
@@ -1,18 +1,48 @@
 <template>
   <page-wrapper withPadding>
-    <article-page title="Orders">Orders page</article-page>
+    <article-page title="Orders">
+      <p>Orders page</p>
+      <av-button
+        :fullWidth="windowWidth < 835"
+        :long="windowWidth > 835"
+        @btn-click="signOut"
+      >Sign Out</av-button>
+    </article-page>
   </page-wrapper>
 </template>
+
+<style scoped>
+p {
+  margin-bottom: 16px;
+}
+</style>
 
 <script lang="ts">
 import Vue from 'vue';
 import PageWrapper from '../components/PageWrapper.vue';
 import ArticlePage from '../components/ArticlePage.vue';
+import AvButton from '../components/AvButton.vue';
+import { mapActions } from 'vuex';
 
 export default Vue.extend({
   components: {
     PageWrapper,
-    ArticlePage
+    ArticlePage,
+    AvButton
+  },
+  data() {
+    return {
+      windowWidth: window.innerWidth
+    };
+  },
+  methods: {
+    ...mapActions('user', ['signOut'])
+  },
+  mounted() {
+    window.addEventListener(
+      'resize',
+      () => (this.windowWidth = window.innerWidth)
+    );
   }
 });
 </script>
