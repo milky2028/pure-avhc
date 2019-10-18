9040f792571ff905e0127d52b5d6781695a09320
Tyler <tylergross28@gmail.com>
2019-10-17 20:36:11 -0500

    wholesale account page base

--- public/src/views/Wholesale.vue
+++ public/src/views/Wholesale.vue
@@ -16,27 +16,75 @@
         >Wholesale Catalog</a>
       </p>
       <h2 class="last">Wholesale Account Application</h2>
-      <shipping-form includeCompany @form-input="shippingForm = $event"></shipping-form>
-      <div class="switch-container">
+      <form v-if="!uid">
+        <av-input
+          dark
+          morePadding
+          type="email"
+          autocomplete="email"
+          placeholder="Email"
+          :pattern="emailPattern"
+          @on-input="userInfo.email = $event"
+          :value="userInfo.email"
+        ></av-input>
+        <av-input
+          dark
+          morePadding
+          placeholder="Phone Number"
+          type="tel"
+          autocomplete="tel"
+          :value="userInfo.phoneNumber"
+          @on-input="userInfo.phoneNumber = $event"
+        ></av-input>
+        <av-input
+          dark
+          morePadding
+          type="password"
+          placeholder="Password"
+          autocomplete="current-password"
+          @on-input="userInfo.password = $event"
+          :value="userInfo.password"
+        ></av-input>
+      </form>
+      <shipping-form v-if="!uid" includeCompany @form-input="shippingForm = $event"></shipping-form>
+      <div v-if="!uid" class="switch-container">
         <av-switch class="switch" @switch="differentBilling = $event"></av-switch>
         <p class="billing-question">Different billing address?</p>
       </div>
-      <shipping-form includeCompany isBilling v-if="differentBilling"></shipping-form>
+      <shipping-form
+        includeCompany
+        isBilling
+        v-if="differentBilling && !uid"
+        @form-input="billingForm = $event"
+      ></shipping-form>
       <p class="errors" v-if="errors.length > 0" :class="{ topMargin: differentBilling }">
         The following fields are required:
         <strong>{{ errors.map((e) => uncamelize(e)).join(', ') }}.</strong>
+      </p>
+      <p class="user-msg" v-if="uid">
+        You are currently signed in with an existing account. You can click the button below to upgrade your account to a wholesale account, or, if you prefer, you can
+        <a
+          @click="signOut"
+        >sign out</a> and create a new wholesale account with a different email.
       </p>
       <av-button
         :class="{ topMargin: differentBilling}"
         :fullWidth="windowWidth < 835"
         :long="windowWidth > 835"
         @btn-click="onSubmit"
-      >Submit</av-button>
+      >{{ uid ? 'Upgrade to Wholesale Account' : 'Submit' }}</av-button>
     </article-page>
   </page-wrapper>
 </template>
 
 <style scoped>
+form {
+  display: grid;
+  grid-auto-flow: row;
+  grid-row-gap: 1vmax;
+  margin: 16px 0;
+}
+
 .last {
   margin-bottom: 15px;
 }
@@ -61,6 +109,11 @@
   color: var(--warn);
   margin-bottom: 16px;
 }
+
+.user-msg {
+  padding-top: 0;
+  margin-bottom: 16px;
+}
 </style>
 
 <script lang="ts">
@@ -70,6 +123,7 @@ import ArticlePage from '../components/ArticlePage.vue';
 import ShippingForm from '../components/ShippingForm.vue';
 import AvSwitch from '../components/AvSwitch.vue';
 import AvButton from '../components/AvButton.vue';
+import AvInput from '../components/AvInput.vue';
 import uncamelize from '../functions/uncamelize';
 import { mapActions, mapState } from 'vuex';
 
@@ -79,18 +133,23 @@ export default Vue.extend({
     ShippingForm,
     ArticlePage,
     AvSwitch,
-    AvButton
+    AvButton,
+    AvInput
   },
   data() {
     return {
+      emailPattern: '^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$',
       fullName: process.env.VUE_APP_FULL_NAME,
       differentBilling: false,
       windowWidth: window.innerWidth,
-      shippingForm: {
+      userInfo: {
         email: '',
+        phoneNumber: '',
+        password: ''
+      },
+      shippingForm: {
         name: '',
         company: '',
-        phoneNumber: '',
         address: '',
         city: '',
         state: '',
@@ -110,35 +169,43 @@ export default Vue.extend({
     };
   },
   computed: {
-    ...mapState('base', ['wholesaleCatalog'])
+    ...mapState('base', ['wholesaleCatalog']),
+    ...mapState('user', ['uid'])
   },
   methods: {
     ...mapActions('base', ['getFirestoreData']),
+    ...mapActions('user', ['signOut']),
     uncamelize,
     capitalizeFirstLetter(text: string) {
       return text.replace(/^\w/, (c) => c.toUpperCase());
     },
     onSubmit() {
-      const unrequiredFields = ['company'];
-      const form = {
-        ...this.shippingForm,
-        ...(this.differentBilling ? this.billingForm : {})
-      };
+      if (this.uid) {
+        console.log('upgrade account');
+      } else {
+        const unrequiredFields = ['company'];
+        const userErrors = Object.entries(this.userInfo)
+          .filter(([, value]) => !value)
+          .map(([key]) => key);
 
-      const shippingErrors = Object.entries(this.shippingForm)
-        .filter(([key, value]) => !unrequiredFields.includes(key) && !value)
-        .map(([key]) => key);
+        const shippingErrors = Object.entries(this.shippingForm)
+          .filter(([key, value]) => !unrequiredFields.includes(key) && !value)
+          .map(([key]) => key);
 
-      const billingErrors = Object.entries(this.shippingForm)
-        .filter(([key, value]) => !unrequiredFields.includes(key) && !value)
-        .map(([key]) => `billing${this.capitalizeFirstLetter(key)}`);
+        const billingErrors = Object.entries(this.shippingForm)
+          .filter(([key, value]) => !unrequiredFields.includes(key) && !value)
+          .map(([key]) => `billing${this.capitalizeFirstLetter(key)}`);
 
-      this.errors = [
-        ...shippingErrors,
-        ...(this.differentBilling ? billingErrors : [])
-      ];
+        this.errors = [
+          ...userErrors,
+          ...shippingErrors,
+          ...(this.differentBilling ? billingErrors : [])
+        ];
 
-      console.log(this.shippingForm, this.billingForm);
+        if (this.errors.length === 0) {
+          console.log(this.shippingForm, this.billingForm, this.userInfo);
+        }
+      }
     }
   },
   mounted() {
