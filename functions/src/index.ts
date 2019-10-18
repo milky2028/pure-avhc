26e155496e5a73114f87244aebe7e1e95ab8bf52
Tyler <tylergross28@gmail.com>
2019-10-18 17:25:33 -0500

    cors errors, probably go back to old headers

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,47 @@
 import * as functions from 'firebase-functions';
-// import * as admin from 'firebase-admin';
+import * as admin from 'firebase-admin';
 import axios, { AxiosRequestConfig } from 'axios';
+import WholesaleUserInfo from '../../shared-types/WholesaleUserInfo';
 
 export const createWholesaleUser = functions.https.onRequest(
   async (req, res) => {
     // res.set('Access-Control-Allow-Origin', '*');
     // res.set('Access-Control-Allow-Methods', 'GET');
     // res.set('Access-Control-Allow-Headers', 'Content-Type');
-    // admin.auth().setCustomUserClaims(uid, { isAdmin: true });
     console.log(req.body.uid);
     try {
-      if (req.method === 'OPTIONS') {
+      if (req.method === 'PUT') {
+        console.error('Illegal put request');
+        return res.sendStatus(403);
+      } else if (req.method === 'OPTIONS') {
         return res.sendStatus(204);
       } else {
-        return res.sendStatus(200);
+        const auth = admin.auth();
+        const {
+          isExistingUser,
+          uid,
+          userInfo,
+          shippingAddress,
+          billingAddress
+        }: WholesaleUserInfo = req.body;
+        if (isExistingUser && uid) {
+          await auth.setCustomUserClaims(uid, { isWholesaleUser: true });
+          return res.sendStatus(200);
+        } else if (userInfo) {
+          const user = await auth.createUser(userInfo);
+          await auth.setCustomUserClaims(user.uid, { isWholesaleUser: true });
+          const shippingDoc = { uid: user.uid, ...shippingAddress };
+          const billingDoc = { uid: user.uid, ...billingAddress };
+          const addresses = [billingDoc, shippingDoc];
+          const firestore = admin.firestore();
+          await Promise.all(
+            addresses.map((doc) => firestore.collection('addresses').add(doc))
+          );
+          return res.sendStatus(201);
+        } else {
+          console.error('Missing user info');
+          return res.status(406).send('Missing user info');
+        }
       }
     } catch (e) {
       console.error(e);
