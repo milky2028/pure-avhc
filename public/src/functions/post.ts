3e62cd3058587b80d92df4820dc2de8764dc5e59
Tyler <tylergross28@gmail.com>
2019-10-20 17:58:33 -0500

    better error message

--- public/src/functions/post.ts
+++ public/src/functions/post.ts
@@ -10,7 +10,7 @@ export default async function post(url: string, data: {}) {
     };
     const response = await fetch(url, fetchOptions);
     if (response.status >= 400) {
-      throw new Error(response.statusText);
+      throw new Error(`HTTP Error: ${response.status}: ${response.statusText}`);
     } else {
       return await response.json();
     }
