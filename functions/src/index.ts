9a34776c096fd3522ab09e9ab1800824693e45b1
Tyler <tylergross28@gmail.com>
2019-10-22 17:00:44 -0500

    handle existing user user case

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,19 +2,41 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import axios, { AxiosRequestConfig } from 'axios';
 import WholesaleUserInfo from './types/WholesaleUserInfo';
+import Address from './types/Address';
 import cors from 'cors';
 const c = cors({ origin: true });
 admin.initializeApp();
 
 // For Local Testing
-// console.log(process.env);
 // import serviceAccount from './credentials/key.json';
 // admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
 // });
 
+async function addAddresses(
+  uid: string,
+  shippingAddress: Address,
+  billingAddress?: Address
+) {
+  const shippingDoc = { uid, ...shippingAddress };
+  const billingDoc = { uid, ...billingAddress };
+  const addresses = [billingDoc, shippingDoc];
+  const firestore = admin.firestore();
+  return await Promise.all(
+    addresses.map((doc) => firestore.collection('addresses').add(doc))
+  );
+}
+
 export const createWholesaleUser = functions.https.onRequest((req, res) =>
   c(req, res, async () => {
+    const auth = admin.auth();
+    const {
+      isExistingUser,
+      uid,
+      userInfo,
+      shippingAddress,
+      billingAddress
+    }: WholesaleUserInfo = req.body;
     try {
       if (req.method === 'PUT') {
         console.error('Illegal put request');
@@ -22,29 +44,19 @@ export const createWholesaleUser = functions.https.onRequest((req, res) =>
       } else if (req.method === 'OPTIONS') {
         return res.sendStatus(204);
       } else {
-        const auth = admin.auth();
-        const {
-          isExistingUser,
-          uid,
-          userInfo,
-          shippingAddress,
-          billingAddress
-        }: WholesaleUserInfo = req.body;
         if (isExistingUser && uid) {
           await auth.setCustomUserClaims(uid, { isWholesaleUser: true });
           return res.sendStatus(200);
         } else if (userInfo) {
+          const phoneRegExp = /^\+?[1-9]\d{1,14}$/i;
+          if (!phoneRegExp.test(userInfo.phoneNumber)) {
+            delete userInfo.phoneNumber;
+          }
           const user = await auth.createUser(userInfo);
           await auth.setCustomUserClaims(user.uid, {
             isWholesaleUser: true
           });
-          const shippingDoc = { uid: user.uid, ...shippingAddress };
-          const billingDoc = { uid: user.uid, ...billingAddress };
-          const addresses = [billingDoc, shippingDoc];
-          const firestore = admin.firestore();
-          await Promise.all(
-            addresses.map((doc) => firestore.collection('addresses').add(doc))
-          );
+          await addAddresses(user.uid, shippingAddress, billingAddress);
           return res.sendStatus(201);
         } else {
           console.error('Missing user info');
@@ -52,8 +64,23 @@ export const createWholesaleUser = functions.https.onRequest((req, res) =>
         }
       }
     } catch (e) {
-      console.error(e);
-      return res.status(400).send(e);
+      console.error(e.errorInfo);
+      if (e.errorInfo === 'auth/email-already-exists') {
+        const existingNotLoggedInUser = await auth.getUserByEmail(
+          userInfo.email
+        );
+        await auth.setCustomUserClaims(existingNotLoggedInUser.uid, {
+          isWholesaleUser: true
+        });
+        await addAddresses(
+          existingNotLoggedInUser.uid,
+          shippingAddress,
+          billingAddress
+        );
+        return res.sendStatus(201);
+      } else {
+        return res.status(400).send(e.errorInfo);
+      }
     }
   })
 );
