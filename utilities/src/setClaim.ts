3bbd2140df42672e062d693f3d54cfc58dd1d3f6
Tyler <tylergross28@gmail.com>
2019-10-25 17:00:47 -0500

    set claim utility done!

--- utilities/src/setClaim.ts
+++ utilities/src/setClaim.ts
@@ -1,8 +1,29 @@
-async function main() {
-  console.log('bird');
+import * as admin from 'firebase-admin';
+import serviceAccount from './credentials/key.json';
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
+});
+
+async function main(
+  userId: string,
+  field: string,
+  fieldValue: any,
+  type: string
+) {
+  const convertedValue =
+    type === 'bool'
+      ? /true/i.test(fieldValue)
+      : type === 'number'
+      ? +fieldValue
+      : fieldValue;
+  const auth = admin.auth();
+  await auth.setCustomUserClaims(userId, { [field]: convertedValue });
+  await auth.revokeRefreshTokens(userId);
+  return 'Successfully set custom claim';
 }
 
-main()
+const [, , userId, field, fieldValue, type] = process.argv;
+main(userId, field, fieldValue, type)
   .then((res) => {
     console.log(res);
     process.exit(0);
