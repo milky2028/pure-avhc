61c41b00c1f38e0ad785530fa14365e355b2d2e3
Tyler <tylergross28@gmail.com>
2019-10-25 16:09:36 -0500

    utils setup

---
+++ utilities/setClaim.ts
@@ -0,0 +1,13 @@
+async function main() {
+  console.log('bird');
+}
+
+main()
+  .then((res) => {
+    console.log(res);
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
