18fbae2a76b19fd04fe263bf60a6786d27734b41
Tyler <tylergross28@gmail.com>
2019-10-25 22:32:41 -0500

    this is all cleaned up and it looks real good

--- utilities/src/setClaim.ts
+++ utilities/src/setClaim.ts
@@ -4,26 +4,31 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
 });
 
-async function main(
-  userId: string,
-  field: string,
-  fieldValue: any,
-  type: string
-) {
-  const convertedValue =
-    type === 'bool'
-      ? /true/i.test(fieldValue)
-      : type === 'number'
-      ? +fieldValue
-      : fieldValue;
+async function main(args: { userId: string; field: string; fieldValue: any }) {
+  const errors = Object.entries(args)
+    .filter(([, value]) => value === undefined)
+    .map(([key]) => key);
+
+  if (errors.length > 0) {
+    throw `Missing arguments:\n${errors.join('\n')}`;
+  }
+
+  const { userId, field, fieldValue } = args;
+
+  const convertedValue = !isNaN(+fieldValue)
+    ? +fieldValue
+    : /^true|false$/.test(fieldValue)
+    ? /^true$/.test(fieldValue)
+    : fieldValue;
+
   const auth = admin.auth();
   await auth.setCustomUserClaims(userId, { [field]: convertedValue });
   await auth.revokeRefreshTokens(userId);
-  return 'Successfully set custom claim';
+  return `${field} successfully set to ${convertedValue} for user ${userId}`;
 }
 
-const [, , userId, field, fieldValue, type] = process.argv;
-main(userId, field, fieldValue, type)
+const [, , uid, f, fV] = process.argv;
+main({ userId: uid, field: f, fieldValue: fV })
   .then((res) => {
     console.log(res);
     process.exit(0);
