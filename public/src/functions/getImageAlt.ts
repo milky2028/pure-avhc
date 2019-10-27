9cc183e76bda3172b18305a5942447ca64d9fbcd
Tyler <tylergross28@gmail.com>
2019-10-27 13:49:57 -0400

    return type annotation

--- public/src/functions/getImageAlt.ts
+++ public/src/functions/getImageAlt.ts
@@ -1,6 +1,6 @@
 import Image from '@/types/Image';
 
-export default function getImageAlt(id: string, images: Image[]) {
+export default function getImageAlt(id: string, images: Image[]): string {
   const image = images.find((i) => i.product === id && i.mainImage);
   return image ? image.alt : '';
 }
