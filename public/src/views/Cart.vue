9fd08aac4683e375b3aef66c6e2b5da24089e5f2
Tyler <tylergross28@gmail.com>
2019-10-27 21:05:35 -0400

    show subtotal in cart

--- public/src/views/Cart.vue
+++ public/src/views/Cart.vue
@@ -5,6 +5,11 @@
         <cart-item v-for="item in cartItems" :key="item.id" :cartItem="item"></cart-item>
       </div>
       <p v-else>Your cart is empty.</p>
+      <divider class="divider"></divider>
+      <div class="subtotal-container">
+        <p class="subtotal">Subtotal</p>
+        <p class="subtotal">${{ subtotal }}</p>
+      </div>
     </article-page>
   </page-wrapper>
 </template>
@@ -19,6 +24,20 @@
 p {
   padding-top: 0;
 }
+
+.divider {
+  margin-top: 16px;
+}
+
+.subtotal-container {
+  display: flex;
+  justify-content: space-between;
+}
+
+.subtotal {
+  font-weight: 500;
+  font-size: 20px;
+}
 </style>
 
 <script lang="ts">
@@ -26,14 +45,16 @@ import Vue from 'vue';
 import PageWrapper from '../components/PageWrapper.vue';
 import ArticlePage from '../components/ArticlePage.vue';
 import CartItem from '../components/CartItem.vue';
-import { mapState, mapActions } from 'vuex';
+import Divider from '../components/Divider.vue';
+import { mapState, mapActions, mapGetters } from 'vuex';
 import WorkerFns from '../types/WorkerFns';
 
 export default Vue.extend({
   components: {
     PageWrapper,
     ArticlePage,
-    CartItem
+    CartItem,
+    Divider
   },
   data() {
     return {
@@ -42,7 +63,8 @@ export default Vue.extend({
   },
   computed: {
     ...mapState('cart', ['cartItems']),
-    ...mapState('base', ['products', 'images'])
+    ...mapState('base', ['products', 'images']),
+    ...mapGetters('cart', ['subtotal'])
   },
   methods: {
     ...mapActions('base', ['getFirestoreData'])
