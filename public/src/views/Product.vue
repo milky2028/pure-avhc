9ef5837b9605b7cd63c819761e64fb9667ef2b00
Tyler Gross <tylergross28@gmail.com>
2019-11-09 12:46:24 -0600

    since product base

--- public/src/views/Product.vue
+++ public/src/views/Product.vue
@@ -1,7 +1,7 @@
 <template>
   <page-wrapper withPadding>
-    <article-page title="Single product">
-      <p>Single product page</p>
+    <article-page v-if="product" :title="product.name">
+      <p>{{ product.name }}</p>
     </article-page>
   </page-wrapper>
 </template>
@@ -13,6 +13,9 @@
 import Vue from 'vue';
 import PageWrapper from '../components/PageWrapper.vue';
 import ArticlePage from '../components/ArticlePage.vue';
+import WorkerFns from '../types/WorkerFns';
+import { mapActions, mapState } from 'vuex';
+import Product from '../types/Product';
 
 export default Vue.extend({
   components: {
@@ -21,13 +24,44 @@ export default Vue.extend({
   },
   data() {
     return {
+      product: {} as Product | undefined,
       windowWidth: window.innerWidth
     };
+  },
+  watch: {
+    products(products: Product[]) {
+      this.product = products.find(
+        (p: Product) => p.url === this.$route.params.productName
+      );
+    },
+    $route(to) {
+      this.product = this.products.find(
+        (p: Product) => p.url === to.params.productName
+      );
+    }
+  },
+  computed: {
+    ...mapState('base', ['products'])
+  },
+  methods: {
+    ...mapActions('base', ['getFirestoreData'])
   },
   mounted() {
     window.addEventListener(
       'resize',
       () => (this.windowWidth = window.innerWidth)
+    );
+
+    if (this.products.length < 1) {
+      const productsOptions: WorkerFns = {
+        fn: 'getDocuments',
+        collection: 'products'
+      };
+      this.getFirestoreData(productsOptions);
+    }
+
+    this.product = this.products.find(
+      (p: Product) => p.url === this.$route.params.productName
     );
   }
 });
