045c2d366283689a48c5c1abdbd63ae70c39c83d
Tyler Gross <tylergross28@gmail.com>
2019-11-09 11:53:33 -0600

    no bottom magin on divider

--- public/src/components/Divider.vue
+++ public/src/components/Divider.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="divider"></div>
+  <div class="divider" :class="{ noBottomMargin }"></div>
 </template>
 
 <style scoped>
@@ -8,9 +8,17 @@
   height: 2px;
   margin-bottom: 1vh;
 }
+
+.noBottomMargin {
+  margin-bottom: 0;
+}
 </style>
 
 <script lang="ts">
 import Vue from 'vue';
-export default Vue.extend({});
+export default Vue.extend({
+  props: {
+    noBottomMargin: Boolean
+  }
+});
 </script>
