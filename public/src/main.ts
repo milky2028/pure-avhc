9e73ca11061b48117ba872995ed41fb43d281f67
Tyler <tylergross28@gmail.com>
2019-11-11 17:22:06 -0600

    initial setup

--- public/src/main.ts
+++ public/src/main.ts
@@ -2,9 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import VueCompositionApi from '@vue/composition-api';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+Vue.use(VueCompositionApi);
 
 new Vue({
   router,
