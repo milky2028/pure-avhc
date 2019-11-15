3600d06961c7019ff0c36f726b5fc71edb715141
Tyler <tylergross28@gmail.com>
2019-11-15 15:13:59 -0600

    update eslint

--- public/.eslintrc.js
+++ public/.eslintrc.js
@@ -3,8 +3,10 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/typescript'],
+  extends: ['plugin:vue/recommended', 'eslint:recommended', '@vue/typescript'],
   rules: {
+    quotes: ['error', 'single'],
+    'arrow-parens': ['error', 'always'],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
