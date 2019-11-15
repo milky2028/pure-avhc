e293845cd4ec2ff5d59caea159740749943392df
Tyler Gross <tylergross28@gmail.com>
2019-11-15 08:47:20 -0600

    cdn images... done?

--- public/src/use/cdn-image.ts
+++ public/src/use/cdn-image.ts
@@ -1,36 +1,59 @@
 import AvImage from '@/types/AvImage';
+import WorkerEntry from '../workers/worker.entry';
+import FirebaseWorker from '@/workers/firebase.worker';
+import { Remote, proxy } from 'comlink';
+import { ref, Ref } from '@vue/composition-api';
 
-export default function useCDNImages(images: AvImage[], height: number, width?: number, imageType: string, isBackgroundImage: boolean) {
-  const images = 'thing' // here images are fetched as a promise and return static data;
-  const baseImageData = images.find()
-  const imageUrlBase = 'https://res.cloudinary.com/pure-avhc/image/upload/';
-  const dpr = window.devicePixelRatio;
-  const imageParams = [
-    'f_auto',
-    'q_auto:low',
-    `h_${Math.round(height * dpr)}`,
-    'c_fill',
-    ...(width ? [`w_${Math.round(width * dpr)}`] : [])
-  ];
+export default function useCDNImages() {
+  const images: Ref<AvImage[]> = ref([]);
 
-  `${imageUrlBase}${imageParams.join()}${imageUrl}`;
+  async function loadImages() {
+    // @ts-ignore
+    const _i = await new WorkerEntry();
+    const workerInstance = _i as Remote<FirebaseWorker>;
+    return workerInstance.getDocuments(
+      'images',
+      proxy((imageData) => (images.value = imageData))
+    );
+  }
 
-  return { loadImages }
+  async function getImage(
+    product: string,
+    imageType: string,
+    height: number,
+    isBackgroundImage?: boolean,
+    width?: number
+  ) {
+    if (images.value.length < 1) {
+      await loadImages();
+    }
+
+    const targetImage = images.value.find(
+      (image) => image.product === product && image[imageType]
+    );
+    if (targetImage) {
+      const baseImageUrl = 'https://res.cloudinary.com/pure-avhc/image/upload/';
+      const dpr = window.devicePixelRatio;
+      const imageParams = [
+        'f_auto',
+        'q_auto:low',
+        `h_${Math.round(height * dpr)}`,
+        'c_fill',
+        ...(width ? [`w_${Math.round(width * dpr)}`] : [])
+      ];
+
+      return {
+        url: `${
+          isBackgroundImage ? 'url(' : ''
+        }${baseImageUrl}${imageParams.join()}${targetImage.url}${
+          isBackgroundImage ? ')' : ''
+        }`,
+        alt: targetImage.alt
+      };
+    } else {
+      return;
+    }
+  }
+
+  return { images, loadImages, getImage };
 }
-
- function getImageAlt(id: string, images: Image[]): string {
-  const image = images.find((i) => i.product === id && i.mainImage);
-  return image ? image.alt : '';
-}
-
-getProductUrl(id: string) {
-  const image = this.images.find(
-    (i: Image) => i.product === id && i.toolbarImage
-  );
-  return image ? image.url : '';
-}
-
-
-// Do I use a snapshot? Or a static get? Maybe a snapshot for images. Either way, the final API will be images: ref<AvImage[]>, loadImages, getImage(product: string, type: string): { url: string, alt: string }
-
-// must load images before getting single image is possible
