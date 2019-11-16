8dd15df0934786016ea37cb740c5441c082dc315
Tyler Gross <tylergross28@gmail.com>
2019-11-15 18:34:07 -0600

    no longer necessary

--- public/src/functions/getImageAlt.ts
+++
@@ -1,6 +0,0 @@
-import Image from '@/types/AvImage';
-
-export default function getImageAlt(id: string, images: Image[]): string {
-  const image = images.find((i) => i.product === id && i.mainImage);
-  return image ? image.alt : '';
-}
