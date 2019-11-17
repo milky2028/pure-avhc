74bab3d4c1bd39594117cc61312af6ef1491d228
Tyler <tylergross28@gmail.com>
2019-11-16 19:13:06 -0600

    enforce indentation

--- public/.eslintrc.js
+++ public/.eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
       }
     }],
     '@typescript-eslint/semi': ['error', 'always', { omitLastInOneLineBlock: true }],
+    indent: ['error', 2],
     quotes: ['error', 'single'],
     'arrow-parens': ['error', 'always'],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
