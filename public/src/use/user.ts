f455d396b91410f2a5bd51e3ef0ac1022796fa50
Tyler <tylergross28@gmail.com>
2019-11-17 22:16:24 -0600

    user api pretty much done

---
+++ public/src/use/user.ts
@@ -0,0 +1,115 @@
+import { reactive, toRefs } from '@vue/composition-api';
+import AvUser from '@/types/AvUser';
+import initializeFirebaseApp from '@/functions/initializeFirebaseApp';
+
+export default async function useUser() {
+  async function initializeAuth(firebase: Promise<firebase.app.App>) {
+    const AuthImport = import(/* webpackChunkName: 'auth' */ 'firebase/auth');
+    const app = await firebase;
+    await AuthImport;
+    return app.auth();
+  }
+
+  const firebase = import(/* webpackChunkName: 'firebase' */ 'firebase/app');
+  const app = initializeFirebaseApp(firebase);
+  const authPromise = initializeAuth(app);
+
+  const emptyUser = {
+    isAdmin: false,
+    isWholesaleUser: false,
+    canSubscribe: true,
+    uid: '',
+    email: '',
+    displayName: '',
+    phoneNumber: '',
+    photoURL: ''
+  } as AvUser;
+  const user = reactive(emptyUser);
+
+  function setUser(newUserData: Partial<AvUser>) {
+    for (const [key, value] of Object.entries(newUserData)) {
+      user[key] = value;
+    }
+  }
+
+  async function listenForAuthStateChanges() {
+    const auth = await authPromise;
+    auth.onAuthStateChanged(async (userDetails) => {
+      if (userDetails) {
+        const { email, phoneNumber, displayName, uid, photoURL } = userDetails;
+        const claims = auth.currentUser
+          ? (await auth.currentUser.getIdTokenResult()).claims
+          : null;
+        const isWholesaleUser = claims ? claims.isWholesaleUser : false;
+        const isAdmin = claims && claims.isAdmin ? claims.isAdmin : false;
+        setUser({
+          email,
+          phoneNumber,
+          displayName,
+          uid,
+          photoURL,
+          isAdmin,
+          isWholesaleUser
+        });
+      }
+    });
+  }
+
+  async function createAccountWithEmailAndPassword(
+    email: string,
+    password: string
+  ) {
+    try {
+      const auth = await authPromise;
+      return auth.createUserWithEmailAndPassword(email, password);
+    } catch (e) {
+      if (e.code === 'auth/email-already-in-use') {
+        return signInWithEmail(email, password);
+      } else {
+        throw new Error(e);
+      }
+    }
+  }
+
+  async function signInWithEmail(email: string, password: string) {
+    const auth = await authPromise;
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+
+  // @ts-ignore
+  async function signInWithProvider(provider: string) {
+    const auth = await authPromise;
+    switch (provider) {
+      case 'google': {
+        const google = new (await firebase).auth.GoogleAuthProvider();
+        return await auth.signInWithPopup(google);
+      }
+      case 'facebook': {
+        const facebook = new (await firebase).auth.FacebookAuthProvider();
+        return await auth.signInWithPopup(facebook);
+      }
+    }
+  }
+
+  async function sendPasswordResetEmail(email: string) {
+    const auth = await authPromise;
+    return auth.sendPasswordResetEmail(email);
+  }
+
+  async function signOut() {
+    const auth = await authPromise;
+    setUser(emptyUser);
+    return auth.signOut();
+  }
+
+  return {
+    ...toRefs(user),
+    signInWithEmail,
+    listenForAuthStateChanges,
+    sendPasswordResetEmail,
+    signOut,
+    initializeAuth,
+    createAccountWithEmailAndPassword,
+    signInWithProvider
+  };
+}
