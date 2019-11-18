bb49526f86137b589c31e04e2db4129455dcc17c
Tyler <tylergross28@gmail.com>
2019-11-17 19:35:32 -0600

    remove auth stuff and unecessary catches

--- public/src/workers/firebase.worker.ts
+++ public/src/workers/firebase.worker.ts
@@ -1,31 +1,26 @@
 import { expose } from 'comlink';
 import QueryParams from '@/types/QueryParams';
 import OrderByParams from '@/types/OrderByParams';
-import AvUser from '@/types/AvUser';
 import Collection from '@/types/Collection';
+import initializeFirebaseApp from '../functions/initializeFirebaseApp';
 
 export default class FirebaseWorker {
   private firebase = import('firebase/app');
-  private fb = this.initializeApp();
+  private fb = initializeFirebaseApp(this.firebase);
   private db = this.intializeFirestore(this.fb);
-  private auth = this.initializeAuth(this.fb);
 
   public async addDocument(
     collection: Collection,
     data: any,
     callback?: (id: string) => any
   ) {
-    try {
-      const db = await this.db;
-      const { id } = await db.collection(collection).add({
-        ...data,
-        timestamp: (await this.firebase).firestore.FieldValue.serverTimestamp()
-      });
-      if (callback) {
-        callback(id);
-      }
-    } catch (e) {
-      throw new Error(e);
+    const db = await this.db;
+    const { id } = await db.collection(collection).add({
+      ...data,
+      timestamp: (await this.firebase).firestore.FieldValue.serverTimestamp()
+    });
+    if (callback) {
+      callback(id);
     }
   }
 
@@ -36,20 +31,16 @@ export default class FirebaseWorker {
       data: firebase.firestore.DocumentData | undefined
     ) => any
   ) {
-    try {
-      const db = await this.db;
-      const doc = db.collection(collection).doc(documentId);
-      if (snapshotCallback) {
-        return doc.onSnapshot((snap) => {
-          const data = snap.data();
-          snapshotCallback(data);
-        });
-      } else {
-        const res = await doc.get();
-        return res.data();
-      }
-    } catch (e) {
-      throw new Error(e);
+    const db = await this.db;
+    const doc = db.collection(collection).doc(documentId);
+    if (snapshotCallback) {
+      return doc.onSnapshot((snap) => {
+        const data = snap.data();
+        snapshotCallback(data);
+      });
+    } else {
+      const res = await doc.get();
+      return res.data();
     }
   }
 
@@ -57,30 +48,24 @@ export default class FirebaseWorker {
     collection: Collection,
     snapshotCallback?: (data: any[]) => any
   ) {
-    try {
-      const db = await this.db;
-      const docs = db
-        .collection(collection)
-        .where('site', 'array-contains', process.env.VUE_APP_NAME);
-      if (snapshotCallback) {
-        return docs.onSnapshot((snapshot) => {
-          const data = snapshot.docs.map((doc) => ({
-            ...doc.data(),
-            id: doc.id
-          }));
-          const dataWithTimestamps = this.processTimestamps(data);
-          snapshotCallback(dataWithTimestamps);
-        });
-      } else {
-        const querySnapshot = await docs.get();
-        const data: { id: string; [key: string]: any }[] = [];
-        querySnapshot.forEach((doc) =>
-          data.push({ ...doc.data(), id: doc.id })
-        );
-        return data;
-      }
-    } catch (e) {
-      throw new Error(e);
+    const db = await this.db;
+    const docs = db
+      .collection(collection)
+      .where('site', 'array-contains', process.env.VUE_APP_NAME);
+    if (snapshotCallback) {
+      return docs.onSnapshot((snapshot) => {
+        const data = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id
+        }));
+        const dataWithTimestamps = this.processTimestamps(data);
+        snapshotCallback(dataWithTimestamps);
+      });
+    } else {
+      const querySnapshot = await docs.get();
+      const data: { id: string; [key: string]: any }[] = [];
+      querySnapshot.forEach((doc) => data.push({ ...doc.data(), id: doc.id }));
+      return data;
     }
   }
 
@@ -100,171 +85,60 @@ export default class FirebaseWorker {
     },
     snapshotCallback?: (data: any[]) => any
   ) {
-    try {
-      const db = await this.db;
-      let results:
-        | firebase.firestore.CollectionReference
-        | firebase.firestore.Query = db.collection(collection);
-      if (!dontQueryBySite) {
-        results = results.where(
-          'site',
-          'array-contains',
-          process.env.VUE_APP_NAME
-        );
-      }
-      if (queries) {
-        queries.forEach(
-          ({ fieldPath, operator, compareValue }: QueryParams) => {
-            results = results.where(fieldPath, operator, compareValue);
-          }
-        );
-      }
-      if (orderBy) {
-        results = results.orderBy(orderBy.field, orderBy.direction);
-      }
-      if (limit) {
-        results = results.limit(limit);
-      }
-      if (snapshotCallback) {
-        return results.onSnapshot((snapshot) => {
-          const data = snapshot.docs.map((doc) => ({
-            ...doc.data(),
-            id: doc.id
-          }));
-          const dataWithTimestamps = this.processTimestamps(data);
-          snapshotCallback(dataWithTimestamps);
-        });
-      } else {
-        const querySnapshot = await results.get();
-        const data: { id: string; [key: string]: any }[] = [];
-        querySnapshot.forEach((doc) =>
-          data.push({ ...doc.data(), id: doc.id })
-        );
-        return data;
-      }
-    } catch (e) {
-      throw new Error(e);
+    const db = await this.db;
+    let results:
+      | firebase.firestore.CollectionReference
+      | firebase.firestore.Query = db.collection(collection);
+    if (!dontQueryBySite) {
+      results = results.where(
+        'site',
+        'array-contains',
+        process.env.VUE_APP_NAME
+      );
     }
-  }
-
-  public async signInWithEmail(email: string, password: string) {
-    try {
-      const auth = await this.auth;
-      return auth.signInWithEmailAndPassword(email, password);
-    } catch (e) {
-      throw new Error(e);
-    }
-  }
-
-  public async createAccountWithEmailAndPassword(
-    email: string,
-    password: string
-  ) {
-    try {
-      const auth = await this.auth;
-      return auth.createUserWithEmailAndPassword(email, password);
-    } catch (e) {
-      if (e.code === 'auth/email-already-in-use') {
-        return this.signInWithEmail(email, password);
-      } else {
-        throw new Error(e);
-      }
-    }
-  }
-
-  public async signOut() {
-    try {
-      const auth = await this.auth;
-      return auth.signOut();
-    } catch (e) {
-      throw new Error(e);
-    }
-  }
-
-  public async listenForAuthStateChanges(callback: (user: AvUser) => any) {
-    try {
-      const auth = await this.auth;
-      auth.onAuthStateChanged(async (userDetails) => {
-        if (userDetails) {
-          const {
-            email,
-            phoneNumber,
-            displayName,
-            uid,
-            photoURL
-          } = userDetails;
-          const claims = auth.currentUser
-            ? (await auth.currentUser.getIdTokenResult()).claims
-            : null;
-          const isWholesaleUser = claims ? claims.isWholesaleUser : false;
-          const isAdmin = claims && claims.isAdmin ? claims.isAdmin : false;
-          callback({
-            email,
-            phoneNumber,
-            displayName,
-            uid,
-            photoURL,
-            isWholesaleUser,
-            isAdmin
-          });
-        }
+    if (queries) {
+      queries.forEach(({ fieldPath, operator, compareValue }: QueryParams) => {
+        results = results.where(fieldPath, operator, compareValue);
       });
-    } catch (e) {
-      throw new Error(e);
     }
-  }
-
-  public async sendPasswordResetEmail(email: string) {
-    try {
-      const auth = await this.auth;
-      return auth.sendPasswordResetEmail(email);
-    } catch (e) {
-      throw new Error(e);
+    if (orderBy) {
+      results = results.orderBy(orderBy.field, orderBy.direction);
     }
-  }
-
-  private async initializeAuth(firebase: Promise<firebase.app.App>) {
-    try {
-      const AuthImport = import(/* webpackChunkName: 'auth' */ 'firebase/auth');
-      const app = await firebase;
-      await AuthImport;
-      return app.auth();
-    } catch (e) {
-      throw new Error(e);
+    if (limit) {
+      results = results.limit(limit);
+    }
+    if (snapshotCallback) {
+      return results.onSnapshot((snapshot) => {
+        const data = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id
+        }));
+        const dataWithTimestamps = this.processTimestamps(data);
+        snapshotCallback(dataWithTimestamps);
+      });
+    } else {
+      const querySnapshot = await results.get();
+      const data: { id: string; [key: string]: any }[] = [];
+      querySnapshot.forEach((doc) => data.push({ ...doc.data(), id: doc.id }));
+      return data;
     }
   }
 
   private async intializeFirestore(firebase: Promise<firebase.app.App>) {
-    try {
-      const Firestore = import(
-        /* webpackChunkName: 'firestore' */ 'firebase/firestore'
-      );
-      const app = await firebase;
-      await Firestore;
-      const db = app.firestore();
-      db.enablePersistence().catch((e) => {
-        if (e.code === 'failed-precondition') {
-          throw new Error(e);
-        } else if (e.code === 'unimplemented') {
-          throw new Error(e);
-        }
-      });
-      return db;
-    } catch (e) {
-      throw new Error(e);
-    }
-  }
-
-  private async initializeApp() {
-    try {
-      const fb = await this.firebase;
-      const firebaseConfig = JSON.parse(
-        process.env.VUE_APP_FIREBASE_CONFIG as string
-      );
-      return fb.initializeApp(firebaseConfig);
-    } catch (e) {
-      throw new Error(e);
-    }
+    const Firestore = import(
+      /* webpackChunkName: 'firestore' */ 'firebase/firestore'
+    );
+    const app = await firebase;
+    await Firestore;
+    const db = app.firestore();
+    db.enablePersistence().catch((e) => {
+      if (e.code === 'failed-precondition') {
+        throw new Error(e);
+      } else if (e.code === 'unimplemented') {
+        throw new Error(e);
+      }
+    });
+    return db;
   }
 
   private processTimestamps(data: { [key: string]: any }[]) {
