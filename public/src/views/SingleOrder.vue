7769e01a44ef03969104fc4f5efc73f2c739cdad
Tyler <tylergross28@gmail.com>
2019-11-18 22:32:49 -0600

    easy one

--- public/src/views/SingleOrder.vue
+++ public/src/views/SingleOrder.vue
@@ -9,25 +9,14 @@
 <style scoped></style>
 
 <script lang="ts">
-import Vue from 'vue';
+import { createComponent } from '@vue/composition-api';
 import PageWrapper from '../components/PageWrapper.vue';
 import ArticlePage from '../components/ArticlePage.vue';
 
-export default Vue.extend({
+export default createComponent({
   components: {
     PageWrapper,
     ArticlePage
-  },
-  data() {
-    return {
-      windowWidth: window.innerWidth
-    };
-  },
-  mounted() {
-    window.addEventListener(
-      'resize',
-      () => (this.windowWidth = window.innerWidth)
-    );
   }
 });
 </script>
