c578c47af024a8e74bf81273d4f1cbe0be682eee
Tyler Gross <tylergross28@outlook.com>
2019-11-19 21:00:37 +0000

    soem type stuff

--- public/src/types/MenuItem.ts
+++ public/src/types/MenuItem.ts
@@ -1,18 +1,27 @@
 import { Site } from './Site';
 
 export interface MenuItem {
+  id: string;
+  sortOrder: number;
+  site: Site[];
   display: string;
   type: 'external' | 'internal';
   url: string;
 }
 
-export interface SubmenuItem {
-  sortOrder: number;
+export interface SubmenuItem extends MenuItem {
   alt: string;
   icon: string;
   iconType: 'external' | 'material';
   linkType: 'external' | 'internal';
   action: string;
-  site: Site[];
   url: string;
+}
+
+export interface IconMenuItem extends MenuItem {
+  action: string;
+  alt: string;
+  icon: string;
+  iconType: 'material' | 'external';
+  linkType: 'internal' | 'external';
 }
