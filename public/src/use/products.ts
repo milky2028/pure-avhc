17cb7623ae13672b0728167ef4ba412bf1d7d544
Tyler Gross <tylergross28@outlook.com>
2019-11-19 19:51:31 +0000

    await product load

--- public/src/use/products.ts
+++ public/src/use/products.ts
@@ -17,9 +17,9 @@ export default function useProducts() {
     );
   }
 
-  onMounted(() => {
+  onMounted(async () => {
     if (products.value.length < 1) {
-      loadProducts();
+      await loadProducts();
     }
   });
 
