25bfa464da398eedc79bcf85d9f684066e1114fb
Tyler Gross <tylergross28@gmail.com>
2019-11-19 21:01:58 -0600

    strains

---
+++ public/src/use/strains.ts
@@ -0,0 +1,27 @@
+import WorkerEntry from '../workers/worker.entry';
+import FirebaseWorker from '@/workers/firebase.worker';
+import { Remote, proxy } from 'comlink';
+import { ref, onMounted } from '@vue/composition-api';
+import Strain from '@/types/Strain';
+
+export default function useStrains() {
+  const strains = ref([] as Strain[]);
+
+  async function loadStrains() {
+    // @ts-ignore
+    const _i = await new WorkerEntry();
+    const workerInstance = _i as Remote<FirebaseWorker>;
+    return workerInstance.getDocuments(
+      'products',
+      proxy((wStrains) => (strains.value = wStrains))
+    );
+  }
+
+  onMounted(async () => {
+    if (strains.value.length < 1) {
+      await loadStrains();
+    }
+  });
+
+  return { loadStrains, strains };
+}
