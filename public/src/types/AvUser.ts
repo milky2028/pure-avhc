716cdc26d83736a7e0d129b6c0942355b17de8e3
Tyler Gross <tylergross28@gmail.com>
2019-11-19 21:56:10 -0600

    type fix

--- public/src/types/AvUser.ts
+++ public/src/types/AvUser.ts
@@ -4,7 +4,7 @@ export default interface AvUser {
   [key: string]: any;
   isAdmin: boolean;
   isWholesaleUser: boolean;
-  canSubscribe?: boolean;
+  canSubscribe: boolean;
   uid: string;
   email: string | null;
   displayName: string | null;
