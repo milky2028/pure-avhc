bf7d70f407bc21a254926a327c13199132d69dcd
Tyler Gross <tylergross28@outlook.com>
2019-11-21 21:07:04 +0000

    non class worker

--- public/src/workers/entry.ts
+++ public/src/workers/entry.ts
@@ -1,13 +1,10 @@
 import '../types/WebpackWorker';
 import Worker from 'worker-loader!../workers/firebase.worker';
-import FirebaseWorker from './firebase.worker';
-import { wrap, Remote } from 'comlink';
+import { IFirebaseWorker } from './firebase.worker';
+import { wrap } from 'comlink';
 
 async function createWorkerInstance() {
-  const workerEntryPoint = wrap<FirebaseWorker>(new Worker());
-  // @ts-ignore
-  const _i = await new workerEntryPoint();
-  return _i as Remote<FirebaseWorker>;
+  return wrap<IFirebaseWorker>(new Worker());
 }
 
 const workerInstance = createWorkerInstance();
--- public/src/workers/firebase.worker.ts
+++ public/src/workers/firebase.worker.ts
@@ -4,160 +4,160 @@ import OrderByParams from '@/types/OrderByParams';
 import Collection from '@/types/Collection';
 import initializeFirebaseApp from '../functions/initializeFirebaseApp';
 
-export default class FirebaseWorker {
-  private firebase = import(/* webpackChunkName: 'firebase' */ 'firebase/app');
-  private fb = initializeFirebaseApp(this.firebase);
-  private db = this.intializeFirestore(this.fb);
+const _firebase = import(/* webpackChunkName: 'firebase' */ 'firebase/app');
+const _fb = initializeFirebaseApp(_firebase);
+const _db = intializeFirestore(_fb);
 
-  public async addDocument(
-    collection: Collection,
-    data: any,
-    callback?: (id: string) => any
-  ) {
-    const db = await this.db;
-    const { id } = await db.collection(collection).add({
-      ...data,
-      timestamp: (await this.firebase).firestore.FieldValue.serverTimestamp()
-    });
-    if (callback) {
-      callback(id);
-    }
-  }
-
-  public async getDocumentById(
-    collection: Collection,
-    documentId: string,
-    snapshotCallback?: (
-      data: firebase.firestore.DocumentData | undefined
-    ) => any
-  ) {
-    const db = await this.db;
-    const doc = db.collection(collection).doc(documentId);
-    if (snapshotCallback) {
-      return doc.onSnapshot((snap) => {
-        const data = snap.data();
-        snapshotCallback(data);
-      });
-    } else {
-      const res = await doc.get();
-      const data = res.data();
-      if (data) {
-        return this.processSingleTimestamp(data);
-      } else {
-        return;
-      }
-    }
-  }
-
-  public async getDocuments(
-    collection: Collection,
-    snapshotCallback?: (data: any[]) => any
-  ) {
-    const db = await this.db;
-    const docs = db
-      .collection(collection)
-      .where('site', 'array-contains', process.env.VUE_APP_NAME);
-    if (snapshotCallback) {
-      return docs.onSnapshot((snapshot) => {
-        const data = snapshot.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id
-        }));
-        const dataWithTimestamps = this.processTimestamps(data);
-        snapshotCallback(dataWithTimestamps);
-      });
-    } else {
-      const querySnapshot = await docs.get();
-      const data: { id: string; [key: string]: any }[] = [];
-      querySnapshot.forEach((doc) => data.push({ ...doc.data(), id: doc.id }));
-      return this.processTimestamps(data);
-    }
-  }
-
-  public async queryDocuments(
-    {
-      collection,
-      queries,
-      limit,
-      orderBy,
-      dontQueryBySite
-    }: {
-      collection: Collection;
-      queries?: QueryParams[];
-      limit?: number;
-      orderBy?: OrderByParams;
-      dontQueryBySite?: boolean;
-    },
-    snapshotCallback?: (data: any[]) => any
-  ) {
-    const db = await this.db;
-    let results:
-      | firebase.firestore.CollectionReference
-      | firebase.firestore.Query = db.collection(collection);
-    if (!dontQueryBySite) {
-      results = results.where(
-        'site',
-        'array-contains',
-        process.env.VUE_APP_NAME
-      );
-    }
-    if (queries) {
-      queries.forEach(({ fieldPath, operator, compareValue }: QueryParams) => {
-        results = results.where(fieldPath, operator, compareValue);
-      });
-    }
-    if (orderBy) {
-      results = results.orderBy(orderBy.field, orderBy.direction);
-    }
-    if (limit) {
-      results = results.limit(limit);
-    }
-    if (snapshotCallback) {
-      return results.onSnapshot((snapshot) => {
-        const data = snapshot.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id
-        }));
-        const dataWithTimestamps = this.processTimestamps(data);
-        snapshotCallback(dataWithTimestamps);
-      });
-    } else {
-      const querySnapshot = await results.get();
-      const data: { id: string; [key: string]: any }[] = [];
-      querySnapshot.forEach((doc) => data.push({ ...doc.data(), id: doc.id }));
-      return this.processTimestamps(data);
-    }
-  }
-
-  private async intializeFirestore(firebase: Promise<firebase.app.App>) {
-    const Firestore = import(
-      /* webpackChunkName: 'firestore' */ 'firebase/firestore'
-    );
-    const app = await firebase;
-    await Firestore;
-    const db = app.firestore();
-    db.enablePersistence().catch((e) => {
-      if (e.code === 'failed-precondition') {
-        throw new Error(e);
-      } else if (e.code === 'unimplemented') {
-        throw new Error(e);
-      }
-    });
-    return db;
-  }
-
-  private processTimestamps(data: { [key: string]: any }[]) {
-    return data.map((d) => this.processSingleTimestamp(d));
-  }
-
-  private processSingleTimestamp(data: { [key: string]: any }) {
-    return Object.fromEntries(
-      Object.entries(data).map(([key, value]) => [
-        key,
-        value.toDate ? value.toDate() : value
-      ])
-    );
+async function addDocument(
+  collection: Collection,
+  data: any,
+  callback?: (id: string) => any
+) {
+  const db = await _db;
+  const { id } = await db.collection(collection).add({
+    ...data,
+    timestamp: (await _firebase).firestore.FieldValue.serverTimestamp()
+  });
+  if (callback) {
+    callback(id);
   }
 }
+
+async function getDocumentById(
+  collection: Collection,
+  documentId: string,
+  snapshotCallback?: (data: firebase.firestore.DocumentData | undefined) => any
+) {
+  const db = await _db;
+  const doc = db.collection(collection).doc(documentId);
+  if (snapshotCallback) {
+    return doc.onSnapshot((snap) => {
+      const data = snap.data();
+      snapshotCallback(data);
+    });
+  } else {
+    const res = await doc.get();
+    const data = res.data();
+    if (data) {
+      return processSingleTimestamp(data);
+    } else {
+      return;
+    }
+  }
+}
+
+async function getDocuments(
+  collection: Collection,
+  snapshotCallback?: (data: any[]) => any
+) {
+  const db = await _db;
+  const docs = db
+    .collection(collection)
+    .where('site', 'array-contains', process.env.VUE_APP_NAME);
+  if (snapshotCallback) {
+    return docs.onSnapshot((snapshot) => {
+      const data = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id
+      }));
+      const dataWithTimestamps = processTimestamps(data);
+      snapshotCallback(dataWithTimestamps);
+    });
+  } else {
+    const querySnapshot = await docs.get();
+    const data: { id: string; [key: string]: any }[] = [];
+    querySnapshot.forEach((doc) => data.push({ ...doc.data(), id: doc.id }));
+    return processTimestamps(data);
+  }
+}
+
+async function queryDocuments(
+  {
+    collection,
+    queries,
+    limit,
+    orderBy,
+    dontQueryBySite
+  }: {
+    collection: Collection;
+    queries?: QueryParams[];
+    limit?: number;
+    orderBy?: OrderByParams;
+    dontQueryBySite?: boolean;
+  },
+  snapshotCallback?: (data: any[]) => any
+) {
+  const db = await _db;
+  let results:
+    | firebase.firestore.CollectionReference
+    | firebase.firestore.Query = db.collection(collection);
+  if (!dontQueryBySite) {
+    results = results.where('site', 'array-contains', process.env.VUE_APP_NAME);
+  }
+  if (queries) {
+    queries.forEach(({ fieldPath, operator, compareValue }: QueryParams) => {
+      results = results.where(fieldPath, operator, compareValue);
+    });
+  }
+  if (orderBy) {
+    results = results.orderBy(orderBy.field, orderBy.direction);
+  }
+  if (limit) {
+    results = results.limit(limit);
+  }
+  if (snapshotCallback) {
+    return results.onSnapshot((snapshot) => {
+      const data = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id
+      }));
+      const dataWithTimestamps = processTimestamps(data);
+      snapshotCallback(dataWithTimestamps);
+    });
+  } else {
+    const querySnapshot = await results.get();
+    const data: { id: string; [key: string]: any }[] = [];
+    querySnapshot.forEach((doc) => data.push({ ...doc.data(), id: doc.id }));
+    return processTimestamps(data);
+  }
+}
+
+async function intializeFirestore(firebase: Promise<firebase.app.App>) {
+  const Firestore = import(
+    /* webpackChunkName: 'firestore' */ 'firebase/firestore'
+  );
+  const app = await firebase;
+  await Firestore;
+  const db = app.firestore();
+  db.enablePersistence().catch((e) => {
+    if (e.code === 'failed-precondition') {
+      throw new Error(e);
+    } else if (e.code === 'unimplemented') {
+      throw new Error(e);
+    }
+  });
+  return db;
+}
+
+function processTimestamps(data: { [key: string]: any }[]) {
+  return data.map((d) => processSingleTimestamp(d));
+}
+
+function processSingleTimestamp(data: { [key: string]: any }) {
+  return Object.fromEntries(
+    Object.entries(data).map(([key, value]) => [
+      key,
+      value.toDate ? value.toDate() : value
+    ])
+  );
+}
+
+export type IFirebaseWorker = typeof FirebaseWorker;
+export const FirebaseWorker = {
+  addDocument,
+  queryDocuments,
+  getDocumentById,
+  getDocuments
+};
 
 expose(FirebaseWorker);
