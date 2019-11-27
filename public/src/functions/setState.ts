ff873eaccf38dec1b8b8b4d033ececa54e299ae6
Tyler Gross <tylergross28@gmail.com>
2019-11-27 00:03:38 -0600

    name change

--- public/src/functions/setState.ts
+++ public/src/functions/setState.ts
@@ -11,10 +11,7 @@ export default function setState<M>(state: State, { type, data }: Mutation<M>) {
   return (state[type] = data);
 }
 
-export function setAllStateInObj(
-  state: State,
-  targetObj: { [key: string]: any }
-) {
+export function setAllStateInObj(originalObj: State, targetObj: State) {
   for (const key of Object.keys(targetObj)) {
     const objValue = targetObj[key];
     setState(state, { type: key, data: objValue });
