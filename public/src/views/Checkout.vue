b0f927467db2f04f38f82b50c0fed8927401a818
Tyler <tylergross28@gmail.com>
2019-12-12 15:24:13 -0600

    Checkout page description

--- public/src/views/Checkout.vue
+++ public/src/views/Checkout.vue
@@ -12,11 +12,19 @@
 import PageWrapper from '../components/PageWrapper.vue';
 import ArticlePage from '../components/ArticlePage.vue';
 import { createComponent } from '@vue/composition-api';
+import { useMetadata } from '../use/metadata';
 
 export default createComponent({
   components: {
     PageWrapper,
     ArticlePage
+  },
+  setup() {
+    const { setTitle, setPageDescription } = useMetadata();
+    setTitle('Checkout');
+    setPageDescription(
+      `${process.env.VUE_APP_FULL_NAME}'s Checkout. Here, you can complete your purchase and get ready to have your CBD Flower products shipped directly to your door.`
+    );
   }
 });
 </script>
