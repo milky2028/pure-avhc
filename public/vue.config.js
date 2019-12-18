61b5d75cdf4637244614905c06d0b999f81775a5
Tyler <tylergross28@gmail.com>
2019-12-17 22:52:46 -0600

    newer config

--- public/vue.config.js
+++ public/vue.config.js
@@ -3,10 +3,15 @@ module.exports = {
     config.module.rule('js').exclude.add(/\.worker\.js$/);
     config.output.globalObject('self');
     // if (config.plugins.has('preload')) {
-    //   config.plugin('preload').tap(options => {
-    //     options[0].include = ['anything'];
+    //   config.plugin('preload').tap((options) => {
+    //     options[0] = {
+    //       rel: 'preload',
+    //       include: 'asyncChunks',
+    //       excludeHtmlNames: [],
+    //       fileBlacklist: [/\.map/]
+    //     };
     //     return options;
-    //   })
+    //   });
     // }
   },
   productionSourceMap: false
