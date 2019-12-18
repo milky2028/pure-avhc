9f1760f0012358ca21bec874323f4e406489bd4e
Tyler Gross <tylergross28@outlook.com>
2019-12-18 21:32:59 +0000

    better structured data api

--- public/src/use/structuredData.ts
+++ public/src/use/structuredData.ts
@@ -1,16 +1,24 @@
-export default function useStructuredData() {
-  const structuredData = document.querySelector(
-    '#structuredData'
-  ) as HTMLScriptElement;
+import { onBeforeUnmount } from '@vue/composition-api';
 
+export default function useStructuredData() {
   function setStructuredData(structuredData: { [key: string]: any }) {
-    clearStructuredData();
-    structuredData.textContent = JSON.stringify(structuredData);
+    const structuredDataScript = document.createElement('script');
+    structuredData.type = 'application/ld+json';
+    structuredDataScript.textContent = JSON.stringify(structuredData);
+    const head = document.getElementById('header') as HTMLHeadElement;
+    head.appendChild(structuredDataScript);
   }
 
   function clearStructuredData() {
-    structuredData.textContent = '';
+    const allScripts = document.getElementsByTagName('script')
+    for (const script of allScripts) {
+      if (script.type === 'application/ld+json') {
+        script.remove();
+      }
+    }
   }
+
+  onBeforeUnmount(() => clearStructuredData());
 
   return { setStructuredData, clearStructuredData };
 }
