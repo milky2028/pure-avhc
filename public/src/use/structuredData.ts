db9fc42086786d93dbd182bf16289173983f79f5
Tyler <tylergross28@gmail.com>
2019-12-18 20:42:44 -0600

    structured data fix

--- public/src/use/structuredData.ts
+++ public/src/use/structuredData.ts
@@ -3,7 +3,7 @@ import { onBeforeUnmount } from '@vue/composition-api';
 export default function useStructuredData() {
   function setStructuredData(structuredData: { [key: string]: any }) {
     const structuredDataScript = document.createElement('script');
-    structuredData.type = 'application/ld+json';
+    structuredDataScript.type = 'application/ld+json';
     structuredDataScript.textContent = JSON.stringify(structuredData);
     const head = document.getElementById('header') as HTMLHeadElement;
     head.appendChild(structuredDataScript);
