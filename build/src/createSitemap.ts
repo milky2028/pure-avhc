101d384528b203a396f7b4cf2ad98fcb9b2115cd
Tyler <tylergross28@gmail.com>
2019-12-20 14:27:15 -0600

    all paths calculated

--- build/src/createSitemap.ts
+++ build/src/createSitemap.ts
@@ -1,6 +1,7 @@
 import { readFileSync, readdirSync } from 'fs';
 import dotenv from 'dotenv';
 import path from 'path';
+import axios from 'axios';
 
 const main = async (buildTarget: string) => {
   process.chdir('../');
@@ -17,16 +18,35 @@ const main = async (buildTarget: string) => {
   const appDir = path.join(process.cwd(), 'public');
   dotenv.config({ path: path.resolve(appDir, `.env.${buildTarget}prod`) });
   const testFiles = readdirSync(path.join(process.cwd(), 'build/avhc-tests'));
-  const fullPaths = [
-    ...allMatches,
-    ...testFiles.map((testFile) => `/test-results-files/${testFile}`)
-  ]
+  const nestedAsyncPaths = allMatches
     .filter(
       (matchedPath) => !matchedPath.includes('order') && matchedPath !== '*'
     )
-    .map(
-      (matchedPath) => `https://${process.env.VUE_APP_SITE_URL}${matchedPath}`
-    );
+    .map(async (matchedPath) => {
+      if (matchedPath.includes(':')) {
+        const pathChunks = matchedPath.split('/');
+        const collection = pathChunks[1];
+        const databaseUrl = `https://firestore.googleapis.com/v1/projects/aspe-f5783/databases/(default)/documents/${collection}/`;
+        const collectionResult = await axios.get(databaseUrl);
+        return collectionResult.data.documents.map(
+          ({
+            fields: {
+              url: { stringValue }
+            }
+          }: {
+            fields: { url: { stringValue: string } };
+          }) => `/${collection}/${stringValue}`
+        );
+      } else {
+        return matchedPath;
+      }
+    });
+  const fullPaths = [
+    ...(await Promise.all(nestedAsyncPaths)).flat(),
+    ...testFiles.map((testFile) => `/test-results-files/${testFile}`)
+  ].map(
+    (matchedPath) => `https://${process.env.VUE_APP_SITE_URL}${matchedPath}`
+  );
   console.log(fullPaths);
 
   return 'Successfuly created sitemap';
