b83b6c6009fa5a7868a41c2249206f8ec59d2c98
Tyler <tylergross28@gmail.com>
2019-12-22 09:37:10 -0600

    fix broken html

--- functions/src/functions/dynamicRenderer.ts
+++ functions/src/functions/dynamicRenderer.ts
@@ -28,14 +28,19 @@ function isBot(userAgent: string) {
 const dynamicRenderer = functions.https.onRequest((req, res) => {
   const userAgent = req.headers['user-agent'];
   return res.status(200).send(`
-    <!doctype html>
-      <head>
-        <title>Dummy Dynamic Render Page</title>
-      </head>
-      <body>
-        <p>Are you a bot?</>
-        <p>${isBot(userAgent as string)}</p>
-      </body>
+  <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Document</title>
+    </head>
+    <body>
+      <p>Are you a bot?</>
+      <p>${isBot(userAgent as string)}</p>
+    </body>
+    </html>
   `);
   // if (userAgent && isBot(userAgent)) {
   // } else {
