fb6dc1d90d0aa980762fe393fce9d0f74bd277ec
Tyler Gross <tylergross28@outlook.com>
2019-12-24 17:34:38 +0000

    Some other change

--- functions/src/functions/dynamicRenderer.ts
+++ functions/src/functions/dynamicRenderer.ts
@@ -42,7 +42,7 @@ const dynamicRenderer = functions.https.onRequest((req, res) => {
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Document</title>
+      <title>Dynamic Renderer</title>
     </head>
     <body>
       <p>Are you a bot?</p>
