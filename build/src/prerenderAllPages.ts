5c35550a773db34bc3baafabe0ff503eac4213f4
Tyler <tylergross28@gmail.com>
2019-12-26 10:59:16 -0600

    prerending markup, but css is missing=

--- build/src/prerenderAllPages.ts
+++ build/src/prerenderAllPages.ts
@@ -1,29 +1,42 @@
-// import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer';
 import axios from 'axios';
 import dotenv from 'dotenv';
 import path from 'path';
+import { xml2js, ElementCompact } from 'xml-js';
 
-// async function prerenderPage(targetPage: string) {
-//   const browser = await puppeteer.launch({ headless: true });
-//   const browserPage = await browser.newPage();
-// }
+async function prerenderPage(targetPage: string) {
+  const browser = await puppeteer.launch({ headless: true });
+  const browserPage = await browser.newPage();
+  await browserPage.goto(targetPage, { waitUntil: 'networkidle0' });
+  const html = await browserPage.content();
+  await browser.close();
+  return html;
+}
 
-async function getSitemap(targetSite: string) {
+async function getUrlList(targetSite: string) {
   const xmlSitemap = await axios.get(`https://${targetSite}/sitemap.xml`);
-  return xmlSitemap.data;
+  const sitemapAsJs: ElementCompact = xml2js(xmlSitemap.data, {
+    compact: true
+  });
+  return sitemapAsJs.urlset.url.map(
+    (url: { loc: { _text: string } }) => url.loc._text
+  );
 }
 
 const main = async (buildTarget: string) => {
   if (!buildTargetArg) {
     throw new Error('Failed to provide build target');
   }
+  process.chdir('../');
 
   const appDir = path.join(process.cwd(), 'public');
   dotenv.config({
     path: path.resolve(appDir, `.env.${buildTarget}prod`)
   });
-  const sitemap = await getSitemap(process.env.VUE_APP_SITE_URL as string);
-  return sitemap;
+  const allSiteUrls: string[] = await getUrlList(
+    process.env.VUE_APP_SITE_URL as string
+  );
+  return prerenderPage(allSiteUrls[9]);
 };
 
 const buildTargetArg = process.argv[2];
