67eb2fbdc4fd626f77a7da91b74c9e3551aebd04
Tyler <tylergross28@gmail.com>
2019-12-29 18:08:03 -0600

    try to send original page if not a bot

--- functions/src/functions/dynamicRenderer.ts
+++ functions/src/functions/dynamicRenderer.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
+import axios from 'axios';
 
 function isBot(userAgent: string) {
   const bots = [
@@ -33,28 +34,31 @@ function isBot(userAgent: string) {
   return false;
 }
 
-const dynamicRenderer = functions.https.onRequest((req, res) => {
+const dynamicRenderer = functions.https.onRequest(async (req, res) => {
   const userAgent = req.headers['user-agent'];
-  console.log(userAgent);
   // res.set('Cache-Control', 'public, max-age=86400, s-maxage=86400');
-  return res.status(200).send(`
-  <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Dynamic Renderer</title>
-    </head>
-    <body>
-      <p>Are you a bot?</p>
-      <p>${isBot(userAgent as string)}</p>
-    </body>
-    </html>
-  `);
-  // if (userAgent && isBot(userAgent)) {
-  // } else {
-  // }
+  if (userAgent && isBot(userAgent)) {
+    return res.status(200).send(`
+      <!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+          <title>Dynamic Renderer</title>
+        </head>
+        <body>
+          <p>Are you a bot?</p>
+          <p>${isBot(userAgent as string)}</p>
+        </body>
+        </html>
+      `);
+  } else {
+    const pageResponse = await axios.get(
+      `https://${req.headers.host}/${req.originalUrl}`
+    );
+    return res.status(200).send(pageResponse.data);
+  }
 });
 
 export default dynamicRenderer;
