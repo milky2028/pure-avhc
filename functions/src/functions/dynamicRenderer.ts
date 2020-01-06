e0c1baf32066f45494fb3dd85b9c7df2bf96bf39
Tyler <tylergross28@gmail.com>
2020-01-05 22:08:09 -0600

    formatter

--- functions/src/functions/dynamicRenderer.ts
+++ functions/src/functions/dynamicRenderer.ts
@@ -1,28 +1,28 @@
-import * as functions from "firebase-functions";
-import axios from "axios";
-import * as admin from "firebase-admin";
+import * as functions from 'firebase-functions';
+import axios from 'axios';
+import * as admin from 'firebase-admin';
 
 function isBot(userAgent: string) {
   const bots = [
     // search engine crawler bots
-    "googlebot",
-    "google-structured-data-testing-tool",
-    "bingbot",
-    "yandexbot",
-    "duckduckbot",
-    "slurp",
+    'googlebot',
+    'google-structured-data-testing-tool',
+    'bingbot',
+    'yandexbot',
+    'duckduckbot',
+    'slurp',
     // social media link bots
-    "twitterbot",
-    "facebookexternalhit",
-    "linkedinbot",
-    "embedly",
-    "baiduspider",
-    "pinterest",
-    "slackbot",
-    "vkshare",
-    "facebot",
-    "outbrain",
-    "w3c_validator"
+    'twitterbot',
+    'facebookexternalhit',
+    'linkedinbot',
+    'embedly',
+    'baiduspider',
+    'pinterest',
+    'slackbot',
+    'vkshare',
+    'facebot',
+    'outbrain',
+    'w3c_validator'
   ];
 
   for (const bot of bots) {
@@ -37,17 +37,17 @@ function isBot(userAgent: string) {
 const dynamicRenderer = (functionTarget: string) =>
   functions.https.onRequest(async (req, res) => {
     const domain =
-      functionTarget === "avhc"
-        ? "aspenvalleycbd.com"
-        : "pure-e0325.firebaseapp.com";
-    const userAgent = req.headers["user-agent"];
+      functionTarget === 'avhc'
+        ? 'aspenvalleycbd.com'
+        : 'pure-e0325.firebaseapp.com';
+    const userAgent = req.headers['user-agent'];
     // res.set('Cache-Control', 'public, max-age=31536000, s-maxage=86400');
     // res.set('Vary', 'User-Agent');
     if (userAgent && isBot(userAgent)) {
       const pageRenderData = (
         await admin
           .firestore()
-          .collection("pageRenders")
+          .collection('pageRenders')
           .doc(encodeURIComponent(`https://${domain}${req.originalUrl}`))
           .get()
       ).data();
