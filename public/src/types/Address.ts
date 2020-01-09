2303fa7ff321f6f8a51d0ec88f593442a0d7553c
Tyler <tylergross28@gmail.com>
2020-01-08 20:14:28 -0600

    address types updated

--- public/src/types/Address.ts
+++ public/src/types/Address.ts
@@ -1,14 +1,15 @@
 export default interface Address {
-  isBilling: string;
+  isBilling: boolean;
   uid: string;
   enabled: boolean;
   dateCreated: Date;
   dateModified: Date;
   name: string;
   company: string;
-  address: string;
+  address1: string;
+  address2: string;
   city: string;
   state: string;
-  zipCode: number;
+  zipCode: number | string;
   country: string;
 }
