5a61777fc9286c20e805b0e6870d8131d8a52aeb
Tyler <tylergross28@gmail.com>
2020-01-12 16:37:47 -0600

    local build updated

--- build/build.sh
+++ build/build.sh
@@ -43,11 +43,17 @@ yarn install
 yarn build:${BUILD_TARGET} || error_exit "App build failed"
 
 cd ..
+nvm use 12
+rm -rf node_modules
+yarn install
+echo "Building service worker..."
+yarn build-sw || error_exit "Service worker build failed"
+echo "Service worker built"
+
 firebase use ${BUILD_TARGET}
 firebase deploy --only functions || error_exit "Functions deploy failed"
 firebase deploy --only firestore || error_exit "Firestore deploy failed"
 firebase deploy --only hosting:${BUILD_TARGET} || error_exit "Hosting deploy failed"
 
-nvm use 12
 cd build
 yarn prerender-all-pages ${BUILD_TARGET} || error_exit "Prerender failed"
