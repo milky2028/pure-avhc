6186465814eb2ff68c3d72e16618b57501ad6975
Tyler <tylergross28@gmail.com>
2020-01-13 23:51:38 -0600

    eslint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,27 +3,37 @@ module.exports = {
   env: {
     node: true
   },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 7,
-    sourceType: 'module'
-  },
-  plugins: [
-    '@typescript-eslint',
-  ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/recommended',
+    '@vue/prettier/@typescript-eslint',
+    '@vue/typescript/recommended',
+    '@vue/prettier'
   ],
   rules: {
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
     '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/indent': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/camelcase': 'off',
-    // 'prettier/prettier': ['error', { singleQuote: true, arrowParens: 'always' }],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always'
+        }
+      }
+    ],
+    '@typescript-eslint/semi': [
+      'error',
+      'always',
+      { omitLastInOneLineBlock: true }
+    ],
+    'prettier/prettier': [
+      'error',
+      { singleQuote: true, arrowParens: 'always' }
+    ],
     quotes: ['error', 'single', { avoidEscape: true }],
     'no-shadow': ['error', { builtinGlobals: true, hoist: 'all' }],
     'no-useless-catch': 'error',
@@ -31,5 +41,8 @@ module.exports = {
     'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1, maxBOF: 0 }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
   }
 };
