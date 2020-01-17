bac635e5129bb418139e1724498060bf44f4f09e
Tyler Gross <tylergross28@outlook.com>
2020-01-17 15:43:08 +0000

    I accidentally broke this

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:vue/recommended',
+    '@vue/prettier/@typescript-eslint',
+    '@vue/typescript/recommended',
+    '@vue/prettier'
+  ],
+  rules: {
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always'
+        }
+      }
+    ],
+    '@typescript-eslint/semi': [
+      'error',
+      'always',
+      { omitLastInOneLineBlock: true }
+    ],
+    'prettier/prettier': [
+      'error',
+      { singleQuote: true, arrowParens: 'always' }
+    ],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'no-shadow': ['error', { builtinGlobals: true, hoist: 'all' }],
+    'no-useless-catch': 'error',
+    'arrow-parens': ['error', 'always'],
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1, maxBOF: 0 }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  }
+};
