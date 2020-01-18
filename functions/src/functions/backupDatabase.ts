4b5c4417f399a9c750680015754c819e6d37bd76
Tyler <tylergross28@gmail.com>
2020-01-17 20:42:17 -0600

    use cloud run

--- functions/src/functions/backupDatabase.ts
+++ functions/src/functions/backupDatabase.ts
@@ -1,30 +1,33 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+
 const client = new admin.firestore.v1.FirestoreAdminClient();
+const bucket = 'gs://avhc-database-backups';
+const backupDatabase = functions.pubsub
+  .schedule('0 4 * * *')
+  .onRun(async () => {
+    const databaseName = client.databasePath(
+      process.env.GCP_PROJECT,
+      '(default)'
+    );
 
-const backupDatabase = functions.https.onRequest(async (_, res) => {
-  const databaseName = client.databasePath(
-    process.env.GCLOUD_PROJECT,
-    '(default)'
-  );
+    try {
+      const responses = await client.exportDocuments({
+        name: databaseName,
+        outputUriPrefix: bucket,
+        // Leave collectionIds empty to export all collections
+        // or set to a list of collection IDs to export,
+        // collectionIds: ['users', 'posts']
+        collectionIds: []
+      });
 
-  try {
-    const responses = await client.exportDocuments({
-      name: databaseName,
-      outputUriPrefix: 'gs://aspe-f5783.appspot.com/backups',
-      // Leave collectionIds empty to export all collections
-      // or set to a list of collection IDs to export,
-      // collectionIds: ['users', 'posts']
-      collectionIds: []
-    });
-
-    const response = responses[0];
-    console.log(`Operation Name: ${response['name']}`);
-    return res.status(200).send('Backup operation successful');
-  } catch (e) {
-    console.error(e);
-    throw new Error('Export operation failed');
-  }
-});
+      const response = responses[0];
+      console.log(`Operation Name: ${response['name']}`);
+      return response;
+    } catch (e) {
+      console.error(e);
+      throw new Error('Export operation failed');
+    }
+  });
 
 export default backupDatabase;
