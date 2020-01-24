e1193aaf8b5294a4e4ac95059fbf5c67434d0c9e
Tyler Gross <tylergross28@gmail.com>
2020-01-23 23:05:11 -0600

    maybe getting closer to better bundling

--- public/vue.config.js
+++ public/vue.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   chainWebpack: (config) => {
-    // config.module.rule('js').exclude.add(/\.worker\.js$/);
     config.output.globalObject('self');
     // if (config.plugins.has('preload')) {
     //   config.plugin('preload').tap((options) => {
     //     options[0] = {
     //       rel: 'preload',
-    //       include: 'asyncChunks',
-    //       excludeHtmlNames: [],
-    //       fileBlacklist: [/\.map/]
+    //       include: {
+    //         type: 'initial'
+    //       },
+    //       fileBlacklist: [/\.map$/, /hot-update\.js$/]
     //     };
     //     return options;
     //   });
