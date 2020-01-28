f1475cf6dfdcbf64dfe7c22f51ccd768e009dc0b
Tyler Gross <tylergross28@outlook.com>
2020-01-28 20:47:29 +0000

    match functions eslint

--- sw/.eslintrc.js
+++ sw/.eslintrc.js
@@ -3,21 +3,27 @@ module.exports = {
   env: {
     node: true
   },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   rules: {
     '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/indent': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/semi': [
-      'error',
-      'always',
-      { omitLastInOneLineBlock: true }
-    ],
-    'prettier/prettier': ['error', { singleQuote: true, arrowParens: 'always' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    // 'prettier/prettier': ['error', { singleQuote: true, arrowParens: 'always' }],
     quotes: ['error', 'single', { avoidEscape: true }],
     'no-shadow': ['error', { builtinGlobals: true, hoist: 'all' }],
     'no-useless-catch': 'error',
@@ -25,8 +31,5 @@ module.exports = {
     'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1, maxBOF: 0 }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: '@typescript-eslint/parser'
   }
 };
