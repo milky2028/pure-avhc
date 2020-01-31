4484ede7d5e482f6aa0b2e8fb77979840a3b5bca
Tyler <tylergross28@gmail.com>
2020-01-30 20:18:18 -0600

    items beginning

--- public/src/views/Checkout.vue
+++ public/src/views/Checkout.vue
@@ -83,6 +83,40 @@
           <div>Expanded content</div>
         </template>
       </CollapsableSection>
+      <div class="itemized-total-container">
+        <p class="subhead itemized">Items</p>
+        <div class="money">
+          <div class="items">
+            <div
+              v-for="{
+                id,
+                size,
+                strain,
+                price,
+                quantity,
+                product
+              } in cartItems"
+              :key="id"
+              class="individual-item"
+            >
+              <p class="item-details smaller-font">
+                {{ findProductType(product) }} | {{ size }}
+              </p>
+              <p class="item-details smaller-font">{{ strain }}</p>
+              <p class="item-details">{{ quantity }} x ${{ price }}</p>
+            </div>
+          </div>
+        </div>
+        <p class="subhead itemized">Discounts</p>
+        <p class="subhead itemized money">$35.00</p>
+        <p class="subhead itemized">Shipping</p>
+        <p class="subhead itemized money">$35.00</p>
+        <p class="subhead itemized">Tax</p>
+        <p class="subhead itemized money">$35.00</p>
+        <Divider class="divider" />
+        <p class="subhead itemized">Total</p>
+        <p class="subhead itemized money">$35.00</p>
+      </div>
     </ArticlePage>
   </PageWrapper>
 </template>
@@ -110,6 +144,49 @@ h2 {
 .margin-bottom {
   margin-bottom: 1rem;
 }
+
+.itemized-total-container {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: repeat(5, min-content);
+  grid-template-areas:
+    'cell1 cell2'
+    'cell3 cell4'
+    'cell5 cell6'
+    'cell7 cell8'
+    'divider divider'
+    'cell9 cell10';
+}
+
+.individual-item {
+  text-align: right;
+}
+
+.divider {
+  grid-area: divider;
+  width: calc(100% + 16px);
+}
+
+.itemized {
+  font-weight: 500;
+  padding: 1rem 0;
+}
+
+.items {
+  padding: 1rem 0;
+}
+
+.item-details {
+  padding: 0;
+}
+
+.smaller-font {
+  font-size: 2rem;
+}
+
+.money {
+  justify-self: end;
+}
 </style>
 
 <script lang="ts">
@@ -125,6 +202,9 @@ import { Modules } from '../use/store';
 import { IOrders } from '../use/orders';
 import setAllStateInObj from '../functions/setState';
 import { IFormErrors, useFormErrors } from '../use/form-errors';
+import Divider from '../components/Divider.vue';
+import { ICart } from '../use/cart';
+import { IProducts } from '../use/products';
 
 export default createComponent({
   components: {
@@ -133,7 +213,8 @@ export default createComponent({
     CollapsableSection,
     ShippingForm,
     AvSwitch,
-    AddressDisplay
+    AddressDisplay,
+    Divider
   },
   setup() {
     const { setTitle, setPageDescription } = useMetadata();
@@ -141,6 +222,14 @@ export default createComponent({
     setPageDescription(
       `${process.env.VUE_APP_FULL_NAME}'s Checkout. Here, you can complete your purchase and get ready to have your CBD Flower products shipped directly to your door.`
     );
+
+    const { cartItems } = inject(Modules.cart) as ICart;
+    const { products } = inject(Modules.products) as IProducts;
+
+    function findProductType(pId: string) {
+      const p = products.value.find(({ id }) => pId === id);
+      return p ? p.url : pId;
+    }
 
     const {
       shippingAddress,
@@ -167,6 +256,8 @@ export default createComponent({
     }
 
     return {
+      findProductType,
+      cartItems,
       billingErrors,
       shippingErrors,
       onContinue,
