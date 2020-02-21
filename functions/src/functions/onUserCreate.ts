0c11afb6a9f51ba9ea13a0101a0ee3e31290b2f1
Tyler <tylergross28@gmail.com>
2020-02-21 17:42:30 -0600

    do some essential things when a user is created

--- functions/src/functions/onUserCreate.ts
+++ functions/src/functions/onUserCreate.ts
@@ -1,5 +1,30 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-const onUserCreate = functions.auth.user().onCreate((user) => console.log(user));
+async function createKey() {
+  const alg = { name: 'AES-CTR', length: 256 };
+  const encrpytionMethods = ['encrypt', 'decrypt'];
+  const key = await crypto.subtle.generateKey(alg, true, encrpytionMethods);
+  return crypto.subtle.exportKey('jwk', key);
+}
+
+const onUserCreate = functions.auth.user().onCreate(async (user) => {
+  const firestore = admin.firestore();
+  const userDoc = firestore.collection('userExtras').doc(user.uid);
+
+  await userDoc.set({
+    cart: [],
+    canSubscribe: true,
+    userCreated: admin.firestore.FieldValue.serverTimestamp()
+  });
+
+  const auth = admin.auth();
+  return auth.setCustomUserClaims(user.uid, {
+    isAdmin: false,
+    isWholesaleUser: false,
+    keyCreated: new Date().toJSON(),
+    key: JSON.stringify(await createKey())
+  });
+});
 
 export default onUserCreate;
