8ef34e27344907ec6bedef2514f13cef90f4a13e
Tyler <tylergross28@gmail.com>
2020-03-26 21:46:58 -0500

    new home page!

--- public/src/views/Home.vue
+++ public/src/views/Home.vue
@@ -1,40 +1,10 @@
 <template>
   <PageWrapper>
-    <div class="home">
-      <div
-        v-for="product in products.filter((p) => p.featuredOnHome)"
-        :key="product.name"
-        class="image"
-        :class="{
-          main: product.sortOrder === 0,
-          side1: product.sortOrder === 1,
-          side2: product.sortOrder > 1
-        }"
-        :style="getBackground(product.id)"
-        :name="getAlt(product.id)"
-      >
-        <div class="tagline-container">
-          <h1 class="subhead larger-font">
-            {{ product.tagline }}
-          </h1>
-          <h2 class="body-text subtag">
-            <span>{{ product.subtag }}</span>
-          </h2>
-          <AvButton
-            flat
-            class="cart-btn"
-            @btn-click="$router.push(`/products/${product.url}`)"
-          >
-            Shop {{ getLastWord(product) }} >
-          </AvButton>
-        </div>
-        <AvButton
-          v-if="product.sortOrder === 0"
-          long
-          :full-width="windowWidth < 835"
-          class="btn"
-          @btn-click="$router.push('/shop-cbd')"
-        >
+    <div class="bkg-container">
+      <img :src="src" :alt="alt" />
+      <div class="content-container">
+        <h1>{{ tagline }}</h1>
+        <AvButton long class="btn" @btn-click="$router.push('/shop-cbd')">
           Shop All Products
         </AvButton>
       </div>
@@ -43,93 +13,55 @@
 </template>
 
 <style scoped>
-.home {
+.bkg-container {
   height: calc(100vh - 55px);
   display: grid;
-  grid-template-columns: 2fr 1fr;
-  grid-template-areas:
-    'main side1'
-    'main side2';
+  grid-template-areas: 'main';
 }
 
-.image {
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: 50% 50%;
-  padding: 2vh;
-}
-
-.main {
+img {
   grid-area: main;
+  height: 100%;
+  width: 100vw;
+  object-fit: cover;
+  object-position: 50% 50%;
+}
+
+.content-container {
+  width: 100%;
+  height: 100%;
   display: grid;
+  gap: 1rem;
+  font-family: var(--mukta-malar);
+  grid-area: main;
+  padding: 3rem 3rem calc(55px + 3rem);
+}
+
+h1 {
+  align-self: end;
+  text-transform: uppercase;
+  font-size: 4rem;
+  font-weight: 800;
+  letter-spacing: 0.5rem;
+  line-height: 1.1;
+  color: var(--snackbar-color);
+  text-shadow: 1px 2px #5bb6ff, 3px 2px #5bb6ff;
 }
 
 .btn {
-  margin-bottom: 8vh;
-  margin-right: 3vw;
-  align-self: end;
-  justify-self: end;
+  align-self: start;
+  justify-self: start;
 }
 
-.tagline-container {
-  justify-self: end;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-}
-
-.larger-font {
-  font-weight: 600;
-  text-align: right;
-  line-height: 1.2;
-}
-
-.subtag {
-  background-color: var(--navy);
-  color: white;
-  margin-block-end: 0;
-  margin-top: 5px;
-  text-align: right;
-  padding: 0 5px;
-  line-height: inherit;
-  border-radius: var(--rounded-corner);
-}
-
-.cart-btn {
-  margin-top: 5px;
-  color: var(--dark-accent);
-}
-
-@media (max-width: 835px) {
-  .home {
-    height: calc(200vh - 110px);
-    grid-auto-flow: row;
-    grid-template-columns: 1fr;
-    grid-template-rows: 2fr 1fr 1fr;
-    grid-template-areas:
-      'main'
-      'side1'
-      'side2';
+@media (max-width: 991px) {
+  h1 {
+    text-align: center;
+    align-self: start;
   }
 
   .btn {
-    margin-bottom: 10vh;
-    margin-right: 0;
+    align-self: end;
     justify-self: center;
-  }
-
-  .larger-font {
-    text-align: center;
-    line-height: 1.2;
-  }
-
-  .subtag {
-    text-align: center;
-  }
-
-  .tagline-container {
-    justify-self: center;
-    align-items: center;
   }
 }
 </style>
@@ -137,12 +69,8 @@
 <script lang="ts">
 import PageWrapper from '../components/PageWrapper.vue';
 import AvButton from '../components/AvButton.vue';
-import Product from '../types/Product';
-import { defineComponent, inject, computed } from '@vue/composition-api';
+import { defineComponent } from '@vue/composition-api';
 import { useWindowWidth } from '../use/window-width';
-import { Modules } from '../use/store';
-import { IProducts } from '../use/products';
-import { IImages } from '../use/cdn-image';
 
 export default defineComponent({
   components: {
@@ -150,58 +78,23 @@ export default defineComponent({
     AvButton
   },
   setup() {
+    const alt = process.env.VUE_APP_HOME_ALT;
     const { windowWidth } = useWindowWidth();
-    const { products } = inject(Modules.products) as IProducts;
 
-    function getLastWord(product: Product) {
-      const words = product.name.split(' ');
-      return words[words.length - 1];
-    }
+    const baseImageUrl = 'https://res.cloudinary.com/pure-avhc/image/upload';
+    const dpr = window.devicePixelRatio;
+    const imageParams = [
+      'f_auto',
+      'q_auto:low',
+      `w_${Math.round(windowWidth.value * dpr)}`
+    ];
+    const src = `${baseImageUrl}/${imageParams.join()}/${
+      process.env.VUE_APP_HOME_SRC
+    }`;
 
-    const { getImage } = inject(Modules.images) as IImages;
+    const tagline = process.env.VUE_APP_HOME_TAGLINE;
 
-    function getImageHeight(sortOrder: number): number {
-      const navHeight = 55;
-      const windowHeight = window.innerHeight;
-      return sortOrder === 0
-        ? windowHeight - navHeight
-        : (windowHeight - navHeight) / 2;
-    }
-
-    const images = computed(() =>
-      products.value.map(({ id, sortOrder }) => ({
-        id,
-        ...(getImage(
-          id,
-          'mainImage',
-          getImageHeight(sortOrder),
-          undefined,
-          true
-        ) as { url: string; alt: string })
-      }))
-    );
-
-    function getAlt(productId: string) {
-      const image = images.value.find(({ id }) => id === productId);
-      return image ? image.alt : '';
-    }
-
-    function getBackground(productId: string) {
-      const image = images.value.find(({ id }) => id === productId);
-      return image
-        ? {
-            backgroundImage: `linear-gradient(180deg, #ffffff 10%, rgba(255, 255, 255, 0) 70%), ${image.url}`
-          }
-        : {};
-    }
-
-    return {
-      products,
-      windowWidth,
-      getBackground,
-      getAlt,
-      getLastWord
-    };
+    return { alt, src, tagline };
   }
 });
 </script>
